d67019ce6d763b983d17b6bd2fb573f61618ea09
Sam Ockman <ockman@penguinpython.com>
2012-06-29 18:23:39 -0400

    working on data_sharing

---
+++ wqflask/wqflask/dataSharing/SharingInfoPage.py
@@ -0,0 +1,58 @@
+# Copyright (C) University of Tennessee Health Science Center, Memphis, TN.
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License
+# as published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Affero General Public License for more details.
+#
+# This program is available from Source Forge: at GeneNetwork Project
+# (sourceforge.net/projects/genenetwork/).
+#
+# Contact Drs. Robert W. Williams and Xiaodong Zhou (2010)
+# at rwilliams@uthsc.edu and xzhou15@uthsc.edu
+#
+#
+#
+# This module is used by GeneNetwork project (www.genenetwork.org)
+#
+# Created by GeneNetwork Core Team 2010/08/10
+#
+# Last updated by GeneNetwork Core Team 2010/10/20
+
+from __future__ import print_function, division
+
+from pprint import pformat as pf
+
+from base.templatePage import templatePage
+from base import webqtlConfig
+from dbFunction import webqtlDatabaseFunction
+import SharingBody
+import SharingInfo
+
+
+#########################################
+#      Sharing Info Page
+#########################################
+class SharingInfoPage(templatePage):
+
+    def __init__(self, fd):
+        templatePage.__init__(self, fd)
+        print("fd is:", pf(fd.__dict__))
+        # Todo: Need a [0] in line below????
+        GN_AccessionId = fd.get('GN_AccessionId')   # Used under search datasharing
+        InfoPageName = fd['database'][0]
+        cursor = webqtlDatabaseFunction.getCursor()
+        if InfoPageName and not GN_AccessionId:
+            sql = "select GN_AccesionId from InfoFiles where InfoPageName = %s"
+            cursor.execute(sql, InfoPageName)
+            GN_AccessionId = cursor.fetchone()
+            url = webqtlConfig.CGIDIR + "main.py?FormID=sharinginfo&GN_AccessionId=%s" % GN_AccessionId
+            self.redirection = url
+        else:
+            sharingInfoObject = SharingInfo.SharingInfo(GN_AccessionId, InfoPageName)
+            self.dict['body'] = sharingInfoObject.getBody(infoupdate="")
--- wqflask/wqflask/views.py
+++ wqflask/wqflask/views.py
@@ -7,6 +7,8 @@ from flask import render_template, request
 from wqflask import search_results
 from wqflask.show_trait import show_trait_page
 
+from wqflask.dataSharing import SharingInfoPage
+
 from base import webqtlFormData
 
 from pprint import pformat as pf
@@ -18,8 +20,12 @@ def index_page():
 
 @app.route("/search")
 def search():
-    the_search = search_results.SearchResultPage(request.args)
-    return render_template("search_result_page.html", **the_search.__dict__)
+    if 'info_database' in request.args:
+        print("Going to data_sharing")
+        data_sharing()
+    else:
+        the_search = search_results.SearchResultPage(request.args)
+        return render_template("search_result_page.html", **the_search.__dict__)
 
 @app.route("/showDatabaseBXD")
 def showDatabaseBXD():
@@ -28,3 +34,12 @@ def showDatabaseBXD():
     template_vars = show_trait_page.ShowTraitPage(fd)
     print("showDatabaseBXD template_vars:", pf(template_vars.__dict__))
     return render_template("trait_data_and_analysis.html", **template_vars.__dict__)
+
+#@app.route("/data_sharing")
+def data_sharing():
+    print("In data_sharing")
+    fd = webqtlFormData.webqtlFormData(request.args)
+    print("Have fd")
+    template_vars = SharingInfoPage.SharingInfoPage(fd)
+    print("Made it to rendering")
+    return render_template("data_sharing.html", **template_vars.__dict__)
