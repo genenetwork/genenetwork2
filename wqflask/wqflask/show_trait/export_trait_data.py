f44e0255f19fc581d4ae9f2ad253f01068508d90
Zachary Sloan <zachary.a.sloan@gmail.com>
2012-10-11 18:04:27 -0500

    Worked with the export code and Sam worked on a redesigned index page

--- wqflask/wqflask/show_trait/export_trait_data.py
+++ wqflask/wqflask/show_trait/export_trait_data.py
@@ -8,8 +8,16 @@ from pprint import pformat as pf
 def export_sample_table(targs):
     #print("* keys0 args is:", targs[0].keys())
     
+    
+    test_export_file = open("/home/zas1024/gene/wqflask/wqflask/show_trait/export_test.txt", "w")
+    
     for key, item in targs.iteritems():
         print("[arrow] key is:", key)
     
     sample_data = json.loads(targs['json_data'])
-    print("sample_data is:", pf(sample_data))
+    
+    print("primary_samples is:", pf(sample_data['primary_samples']))
+     
+    for key in sample_data['primary_samples'][0]:
+        test_export_file.write(key + ",")
+    test_export_file.write("\n")
