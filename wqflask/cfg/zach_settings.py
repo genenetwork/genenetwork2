5bfe4e6c590bf0ade2312536f0f8e3ce055e2c75
Zachary Sloan <zas1024@banana1.(none)>
2013-02-22 22:18:38 +0000

    Added some to notes

--- wqflask/cfg/zach_settings.py
+++ wqflask/cfg/zach_settings.py
@@ -2,4 +2,4 @@ LOGFILE = """/tmp/flask_gn_log"""
 
 TRAP_BAD_REQUEST_ERRORS = True
 
-DB_URI = """mysql://webqtl:webqtl@localhost/db_webqtl_zas1024"""
+DB_URI = """mysql://webqtl:f2ZypIflRM@gn.cazhbciu2y1i.us-east-1.rds.amazonaws.com/db_webqtl"""
