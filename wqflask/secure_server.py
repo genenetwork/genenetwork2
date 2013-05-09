1cf6894c7416f879fcd114ae7358bb7d4169a677
Sam <sam@example.com>
2013-05-09 22:28:22 +0000

    Working on user registration

--- wqflask/secure_server.py
+++ wqflask/secure_server.py
@@ -7,6 +7,11 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.security import Security, SQLAlchemyUserDatastore, UserMixin, RoleMixin
 
 
+# Setup mail
+from flask.ext.mail import Mail
+mail = Mail(app)
+
+
 # Create database connection object
 db = SQLAlchemy(app)
 
@@ -26,13 +31,13 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(255))
     active = db.Column(db.Boolean())
     confirmed_at = db.Column(db.DateTime())
-    
+
     last_login_at = db.Column(db.DateTime())
     current_login_at = db.Column(db.DateTime())
     last_login_ip_= db.Column(db.String(39))
     current_login_ip = db.Column(db.String(39))
     login_count = db.Column(db.Integer())
-    
+
     roles = db.relationship('Role', secondary=roles_users,
                             backref=db.backref('users', lazy='dynamic'))
 
