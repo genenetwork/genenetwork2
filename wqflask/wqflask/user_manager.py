57b00317168eb3a84c489c2613133170a191aef6
Sam <no-emails@example.com>
2013-10-15 02:50:25 -0500

    * Redid mail sending service to set a ping showing it's alive
    * Redid server so it wouldn't start unless mail service recently pinged
    
    * Wrote a bunch of forgot password code
    
    * Refactored sending email about verification and forgotten passwords

--- wqflask/wqflask/user_manager.py
+++ wqflask/wqflask/user_manager.py
@@ -19,8 +19,11 @@ import base64
 
 import simplejson as json
 
-from redis import StrictRedis
-Redis = StrictRedis()
+from sqlalchemy import orm
+
+#from redis import StrictRedis
+import redis
+Redis = redis.StrictRedis()
 
 
 from flask import (Flask, g, render_template, url_for, request, make_response,
@@ -53,7 +56,7 @@ def timestamp():
 
 class UserSession(object):
     cookie_name = 'session_id'
-    
+
     def __init__(self):
         cookie = request.cookies.get(self.cookie_name)
         if not cookie:
@@ -70,13 +73,13 @@ class UserSession(object):
             self.record = Redis.hgetall(self.redis_key)
             print("record is:", self.record)
             self.logged_in = True
-        
-        
+
+
     def delete_session(self):
         # And more importantly delete the redis record
         Redis.delete(self.cookie_name)
         print("At end of delete_session")
-        
+
 @app.before_request
 def before_request():
     g.user_session = UserSession()
@@ -105,22 +108,22 @@ class UserManager(object):
             print("  ID:", dataset.id)
             print("  Confidential:", dataset.check_confidentiality())
         #print("   ---> self.datasets:", self.datasets)
-        
+
 
 class RegisterUser(object):
     def __init__(self, kw):
         self.thank_you_mode = False
         self.errors = []
         self.user = Bunch()
-        
+
         self.user.email_address = kw.get('email_address', '').strip()
         if not (5 <= len(self.user.email_address) <= 50):
             self.errors.append('Email Address needs to be between 5 and 50 characters.')
-            
+
         self.user.full_name = kw.get('full_name', '').strip()
         if not (5 <= len(self.user.full_name) <= 50):
             self.errors.append('Full Name needs to be between 5 and 50 characters.')
-            
+
         self.user.organization = kw.get('organization', '').strip()
         if self.user.organization and not (5 <= len(self.user.organization) <= 50):
             self.errors.append('Organization needs to be empty or between 5 and 50 characters.')
@@ -128,48 +131,49 @@ class RegisterUser(object):
         password = str(kw.get('password', ''))
         if not (6 <= len(password)):
             self.errors.append('Password needs to be at least 6 characters.')
-            
+
         if kw.get('password_confirm') != password:
             self.errors.append("Passwords don't match.")
-        
+
         if self.errors:
             return
-        
+
         print("No errors!")
-        
+
         self.set_password(password)
-        
+
         self.user.registration_info = json.dumps(basic_info(), sort_keys=True)
-        
+
         self.new_user = model.User(**self.user.__dict__)
         db_session.add(self.new_user)
         db_session.commit()
-        
+
         print("Adding verification email to queue")
-        self.send_email_verification()
+        #self.send_email_verification()
+        VerificationEmail(self.new_user)
         print("Added verification email to queue")
-        
+
         self.thank_you_mode = True
-        
-    
+
+
     def set_password(self, password):
         pwfields = Bunch()
-        
+
         pwfields.algorithm = "pbkdf2"
         pwfields.hashfunc = "sha256"
         #hashfunc = getattr(hashlib, pwfields.hashfunc)
-        
-        # Encoding it to base64 makes storing it in json much easier 
+
+        # Encoding it to base64 makes storing it in json much easier
         pwfields.salt = base64.b64encode(os.urandom(32))
 
         # https://forums.lastpass.com/viewtopic.php?t=84104
-        pwfields.iterations = 100000   
+        pwfields.iterations = 100000
         pwfields.keylength = 32
-        
+
         pwfields.created_ts = timestamp()
         # One more check on password length
         assert len(password) >= 6, "Password shouldn't be so short here"
-        
+
         print("pwfields:", vars(pwfields))
         print("locals:", locals())
 
@@ -178,30 +182,41 @@ class RegisterUser(object):
                                 pwfields.iterations,
                                 pwfields.keylength,
                                 pwfields.hashfunc)
-        
+
         pwfields.password = enc_password.password
         pwfields.encrypt_time = enc_password.encrypt_time
-        
+
         self.user.password = json.dumps(pwfields.__dict__,
-                                        sort_keys=True,             
+                                        sort_keys=True,
                                        )
-        
-    def send_email_verification(self):
+
+class VerificationEmail(object):
+    template_name =  "email/verification.txt"
+    key_preface = "verification_code"
+    subject = "GeneNetwork email verification"
+
+    def __init__(self, user):
         verification_code = str(uuid.uuid4())
-        key = "verification_code:" + verification_code
-        
-        data = json.dumps(dict(id=self.new_user.id,
+        key = self.key_preface + "verification_code:" + verification_code
+
+        data = json.dumps(dict(id=user.id,
                                timestamp=timestamp())
                           )
-                          
+
         Redis.set(key, data)
         two_days = 60 * 60 * 24 * 2
-        Redis.expire(key, two_days)  
-        to = self.user.email_address
-        subject = "GeneNetwork email verification"
-        body = render_template("email/verification.txt",
+        Redis.expire(key, two_days)
+        to = user.email_address
+        subject = self.subject
+        body = render_template(self.template_name,
                                verification_code = verification_code)
         send_email(to, subject, body)
+
+class ForgotPasswordEmail(VerificationEmail):
+    template_name = "email/forgot_password.txt"
+    key_preface = "forgot_password_code"
+    subject = "GeneNetwork password reset"
+
 
 class Password(object):
     def __init__(self, unencrypted_password, salt, iterations, keylength, hashfunc):
@@ -215,13 +230,14 @@ class Password(object):
                                           salt, iterations, keylength, hashfunc)
         self.encrypt_time = round(time.time() - start_time, 3)
         print("Creating password took:", self.encrypt_time)
-        
-    
+
+
 def basic_info():
     return dict(timestamp = timestamp(),
                 ip_address = request.remote_addr,
                 user_agent = request.headers.get('User-Agent'))
 
+@app.route("/manage/verify_email")
 def verify_email():
     print("in verify_email request.url is:", request.url)
     verify_url_hmac(request.url)
@@ -232,7 +248,7 @@ def verify_email():
     user = model.User.query.get(data['id'])
     user.confirmed = json.dumps(basic_info(), sort_keys=True)
     db_session.commit()
-                                
+
 def login():
     params = request.form if request.form else request.args
     print("in login params are:", params)
@@ -250,10 +266,10 @@ def login():
         print("\n\nComparing:\n{}\n{}\n".format(encrypted.password, pwfields.password))
         valid = pbkdf2.safe_str_cmp(encrypted.password, pwfields.password)
         print("valid is:", valid)
-        
+
         login_rec = model.Login(user)
-        
-        
+
+
         if valid:
             login_rec.successful = True
             login_rec.session_id = str(uuid.uuid4())
@@ -261,17 +277,17 @@ def login():
             session_id_signature = actual_hmac_creation(login_rec.session_id)
             session_id_signed = login_rec.session_id + ":" + session_id_signature
             print("session_id_signed:", session_id_signed)
-            
+
             session = dict(login_time = time.time(),
                            user_id = user.id,
                            user_email_address = user.email_address)
-            
+
             flash("Thank you for logging in.", "alert-success")
-            
+
             key = "session_id:" + login_rec.session_id
             print("Key when signing:", key)
             Redis.hmset(key, session)
-            
+
             response = make_response(redirect(url_for('index_page')))
             response.set_cookie(UserSession.cookie_name, session_id_signed)
         else:
@@ -281,8 +297,8 @@ def login():
         db_session.add(login_rec)
         db_session.commit()
         return response
- 
-@app.route("/n/logout")     
+
+@app.route("/n/logout")
 def logout():
     print("Logging out...")
     UserSession().delete_session()
@@ -291,15 +307,36 @@ def logout():
     # Delete the cookie
     response.set_cookie(UserSession.cookie_name, '', expires=0)
     return response
-    
-       
+
+
+@app.route("/n/forgot_password")
+def forgot_password():
+    return render_template("new_security/forgot_password.html")
+
+@app.route("/n/forgot_password_submit", methods=('POST',))
+def forgot_password_submit():
+    params = request.form
+    email_address = params['email_address']
+    try:
+        user = model.User.query.filter_by(email_address=email_address).one()
+    except orm.exc.NoResultFound:
+        flash("Couldn't find a user associated with the email address {}. Sorry.".format(
+            email_address))
+        return redirect(url_for("login"))
+    ForgotPasswordEmail(user)
+
+@app.route("/n/password_reset")
+def password_reset():
+    pass
+
+
 ################################# Sign and unsign #####################################
 
 def url_for_hmac(endpoint, **values):
     """Like url_for but adds an hmac at the end to insure the url hasn't been tampered with"""
 
     url = url_for(endpoint, **values)
-    
+
     hm = actual_hmac_creation(url)
     if '?' in url:
         combiner = "&"
@@ -323,10 +360,10 @@ def verify_url_hmac(url):
     hm = actual_hmac_creation(url)
 
     assert hm == hmac, "Unexpected url (stage 3)"
-    
+
 def actual_hmac_creation(stringy):
     """Helper function to create the actual hmac"""
-    
+
     secret = app.config['SECRET_HMAC_CODE']
 
     hmaced = hmac.new(secret, stringy, hashlib.sha1)
@@ -339,14 +376,14 @@ def actual_hmac_creation(stringy):
 app.jinja_env.globals.update(url_for_hmac=url_for_hmac)
 
 #######################################################################################
-        
+
 def send_email(to, subject, body):
     msg = json.dumps(dict(From="no-reply@genenetwork.org",
                      To=to,
                      Subject=subject,
                      Body=body))
     Redis.rpush("mail_queue", msg)
-    
+
 
 #def combined_salt(user_salt):
 #    """Combine the master salt with the user salt...we use two seperate salts so that if the database is compromised, the
@@ -358,7 +395,7 @@ def send_email(to, subject, body):
 #    for x, y in user_salt, secret_salt:
 #        combined = combined + x + y
 #    return combined
-    
+
 
 
 class GroupsManager(object):
@@ -374,9 +411,5 @@ class RolesManager(object):
 
 #class Password(object):
 #    """To generate a master password: dd if=/dev/urandom bs=32 count=1 > master_salt"""
-#    
-#    master_salt = 
-
-
-
-
+#
+#    master_salt =
