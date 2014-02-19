b91219e4180173a96dd7535e40db8d788a60db77
root <root@alexandria.uthsc.edu>
2014-02-19 14:19:19 -0600

    Committer: root <root@alexandria.uthsc.edu> On branch master

--- wqflask/maintenance/dataset/load_phenotypes.py
+++ wqflask/maintenance/dataset/load_phenotypes.py
@@ -4,22 +4,25 @@ import csv
 import utilities
 
 def main(argv):
-    #
+    # config
     config = utilities.get_config(argv[1])
     print "config:"
     for item in config.items('config'):
         print "\t%s" % (str(item))
     #
+    inbredsetid = config.get('config', 'inbredsetid')
+    print "inbredsetid: %s" % inbredsetid
+    # datafile
     datafile = open(config.get('config', 'datafile'), 'r')
     phenotypedata = csv.reader(datafile, delimiter='\t', quotechar='"')
-    for row in phenotypedata:
-        #print row
-        pass
-    #
+    phenotypedata_head = phenotypedata.next()
+    print "phenotypedata head:\n\t%s" % phenotypedata_head
+    # metafile
     metafile = open(config.get('config', 'metafile'), 'r')
     phenotypemeta = csv.reader(metafile, delimiter='\t', quotechar='"')
-    for row in phenotypemeta:
-        print row[1]
+    phenotypemeta_head = phenotypemeta.next()
+    print "phenotypemeta head:\n\t%s" % phenotypemeta_head
+    #
         
 if __name__ == "__main__":
     print "command line arguments:\n\t%s" % sys.argv
