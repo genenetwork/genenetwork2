f6d2368d7a89413a8622123e7e43eecc8bef5882
root <root@alexandria.uthsc.edu>
2014-02-19 12:51:26 -0600

    Committer: root <root@alexandria.uthsc.edu> On branch master

--- wqflask/maintenance/dataset/load_phenotypes.py
+++ wqflask/maintenance/dataset/load_phenotypes.py
@@ -4,9 +4,11 @@ import utilities
 
 def main(argv):
     config = utilities.get_config(argv[1])
-    print config.items('config')
+    print "config:"
+    for item in config.items('config'):
+        print "\t%s" % (str(item))
 
 if __name__ == "__main__":
-    print "command line arguments: %s" % sys.argv
+    print "command line arguments:\n\t%s" % sys.argv
     main(sys.argv)
     print "exit successfully"
