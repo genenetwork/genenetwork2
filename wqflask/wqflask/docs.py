e03a5919a2c8755fd4efd20e78ca79381dec3075
Lei Yan <lei@penguin.uthsc.edu>
2014-12-01 17:18:54 +0000

    Committer: Lei Yan <lei@penguin.uthsc.edu> On branch master

---
+++ wqflask/wqflask/docs.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import, print_function, division
+
+from flask import g
+
+class Docs(object):
+
+    def __init__(self, entry):
+        sql = """
+            SELECT Docs.title, Docs.content
+            FROM Docs
+            WHERE Docs.entry LIKE '%s'
+            """
+        result = g.db.execute(sql % (entry)).fetchone()
+        self.title = result[0]
+        self.content = result[1]
