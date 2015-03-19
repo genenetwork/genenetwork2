d29e957fc01b6e91461109c6da3b3d5b0cef9ec5
DannyArends <Danny.Arends@gmail.com>
2015-03-19 23:03:27 +0100

    This front to back passing of parameters is just weird

--- wqflask/wqflask/views.py
+++ wqflask/wqflask/views.py
@@ -287,8 +287,8 @@ def marker_regression_page():
         'control_marker',
         'control_marker_db',
         'pair_scan'
-        'mapmethod_rqtl'
-        'mapmodel_rqtl'
+        'mapmethod'
+        'mapmodel'
     )
 
     start_vars = {}
