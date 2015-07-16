b5aa5cb11c5f13daa7d416c37cc49f504921b037
Lei Yan <lei@penguin.uthsc.edu>
2015-07-16 20:00:10 +0000

    Committer: Lei Yan <lei@penguin.uthsc.edu> On branch master

--- wqflask/wqflask/gsearch.py
+++ wqflask/wqflask/gsearch.py
@@ -4,7 +4,6 @@ from flask import Flask, g
 
 class GSearch(object):
 
-	# http://gn2_lei.genenetwork.org/gsearch?species=mouse&group=BXD&search_terms=shh
 	def __init__(self, kw):
 		if 'species' in kw and 'group' in kw:
 			self.species = kw['species']
@@ -80,4 +79,34 @@ class GSearch(object):
 				ORDER BY inbredset_name, tissue_name, probesetfreeze_fullname, probeset_name
 				LIMIT 1000
 				""" % (self.species_id, self.terms)
+			self.results = g.db.execute(sql).fetchall()
+		else:
+			self.terms = kw['terms']
+			sql = """
+				SELECT
+				Species.`Name` AS species_name,
+				InbredSet.`Name` AS inbredset_name,
+				Tissue.`Name` AS tissue_name,
+				ProbeSetFreeze.FullName AS probesetfreeze_fullname,
+				ProbeSet.Name AS probeset_name,
+				ProbeSet.Symbol AS probeset_symbol,
+				ProbeSet.`description` AS probeset_description,
+				ProbeSet.Chr AS chr,
+				ProbeSet.Mb AS mb,
+				ProbeSetXRef.Mean AS mean,
+				ProbeSetXRef.LRS AS lrs,
+				ProbeSetXRef.`Locus` AS locus,
+				ProbeSetXRef.`pValue` AS pvalue,
+				ProbeSetXRef.`additive` AS additive
+				FROM Species, InbredSet, ProbeSetXRef, ProbeSet, ProbeFreeze, ProbeSetFreeze, Tissue
+				WHERE InbredSet.`SpeciesId`=Species.`Id`
+				AND ProbeFreeze.InbredSetId=InbredSet.`Id`
+				AND ProbeFreeze.`TissueId`=Tissue.`Id`
+				AND ProbeSetFreeze.ProbeFreezeId=ProbeFreeze.Id
+				AND ( MATCH (ProbeSet.Name,ProbeSet.description,ProbeSet.symbol,alias,GenbankId, UniGeneId, Probe_Target_Description) AGAINST ('%s' IN BOOLEAN MODE) )
+				AND ProbeSet.Id = ProbeSetXRef.ProbeSetId
+				AND ProbeSetXRef.ProbeSetFreezeId=ProbeSetFreeze.Id
+				ORDER BY species_name, inbredset_name, tissue_name, probesetfreeze_fullname, probeset_name
+				LIMIT 1000
+				""" % (self.terms)
 			self.results = g.db.execute(sql).fetchall()
