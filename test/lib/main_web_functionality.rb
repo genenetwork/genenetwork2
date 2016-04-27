4172e8940d2656f9f2a4de6e72e6d114c317d1c0
Pjotr Prins <pjotr.public01@thebird.nl>
2016-04-27 11:25:53 +0000

    test: test for broken links

--- test/lib/main_web_functionality.rb
+++ test/lib/main_web_functionality.rb
@@ -6,13 +6,15 @@ end
 describe MainWebFunctionality do
   before do
     @agent = Mechanize.new
+    @agent.agent.http.ca_file = '/etc/ssl/certs/ca-certificates.crt'
   end
 
   describe MainWebFunctionality do
     it "Get to trait page" do
       page = @agent.get($host)
       form = page.forms[1]
-      form.buttons[0].value.must_equal "Search" 
+      form.buttons[0].value.must_equal "Search"
+
       # http://localhost:5003/search?species=mouse&group=BXD&type=Hippocampus+mRNA&dataset=HC_M2_0606_P&search_terms_or=&search_terms_and=MEAN%3D%2815+16%29+LRS%3D%2823+46%29&FormID=searchResult
       form.fields[2].value = "MEAN=(15 16) LRS=(23 46)"
       form.fields[3].value = "mouse"
@@ -23,12 +25,25 @@ describe MainWebFunctionality do
       probe_link = search_page.links.find { |l| l.text =~ /1435395_s_at/ }
       probe_link.uri.to_s.must_equal "/show_trait?trait_id=1435395_s_at&dataset=HC_M2_0606_P"
       show_trait_page = probe_link.click
+      p show_trait_page
+
       # Get to trait page for 1435395_s_at
       form2 = show_trait_page.forms_with("trait_page")[0]
-      form2.fields[30].name.must_equal  "value:DBA/2J"
-      form2.fields[30].value.must_equal "15.287"
-    end
-  end
-  
-end
+      form2.fields[30].name.must_equal  "variance:C57BL/6J"
+      # form2.fields[30].value.must_equal "15.287"
 
+      # Test every link on the page to check if it's broken or not
+      show_trait_page.links.each do |link|
+        puts link.href
+        if link.href !~ /static\/dbdoc\/Hippocampus/ and link.href !~ /glossary.html|sample_r|grits.eecs.utk.edu|correlationAnnotation.html/
+           # Fetch link, program will crash with exception if link is broken
+           linkpage = @agent.get(link.href)
+          puts "Link to #{link.href} is valid, response code #{linkpage.code}"
+        end
+      end
+
+    end
+
+  end
+
+end
