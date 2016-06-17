10df36b60273d81678f6630c07a2d8e5a6409282
Pjotr Prins <pjotr.public01@thebird.nl>
2016-06-17 06:40:57 +0000

    Testing: check for field on search page

--- test/lib/main_web_functionality.rb
+++ test/lib/main_web_functionality.rb
@@ -10,10 +10,11 @@ describe MainWebFunctionality do
   end
 
   describe MainWebFunctionality do
+
     it "Get to trait page" do
       page = @agent.get($host)
       form = page.forms[1]
-      form.buttons[0].value.must_equal "Search"
+      form.buttons[0].value.must_equal "Search" # main menu is loaded
 
       # http://localhost:5003/search?species=mouse&group=BXD&type=Hippocampus+mRNA&dataset=HC_M2_0606_P&search_terms_or=&search_terms_and=MEAN%3D%2815+16%29+LRS%3D%2823+46%29&FormID=searchResult
       form.fields[2].value = "MEAN=(15 16) LRS=(23 46)"
@@ -28,7 +29,10 @@ describe MainWebFunctionality do
       # p show_trait_page
 
       # Get to trait page for 1435395_s_at
+
       form2 = show_trait_page.forms_with("trait_page")[0]
+      # [name: corr_dataset value: HC_M2_0606_P]
+      form2.fields.select { |fld| fld.name == 'corr_dataset' }.first.value.must_equal 'HC_M2_0606_P'
       if $options[:database] == :small
         form2.fields[30].name.must_equal  "value:DBA/2J"
       else
