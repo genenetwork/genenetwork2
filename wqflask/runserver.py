6db25e3481902c2fef5a39cdc3c1c91854138498
Pjotr Prins <pjotr.public01@thebird.nl>
2016-06-18 09:08:08 +0000

    Runner: updated info

--- wqflask/runserver.py
+++ wqflask/runserver.py
@@ -1,24 +1,11 @@
+# Starts the webserver with the ./bin/genenetwork2 command
+#
+# Please note, running with host set externally below combined with
+# debug mode is a security risk unless you have a firewall setup, e.g.
+#
+# /sbin/iptables -A INPUT -p tcp -i eth0 -s ! 71.236.239.43 --dport 5003 -j DROP
+
 from wqflask import app
-
-# Start the webserver with ./bin/genenetwork2
-#
-# Please note, running with host set externally below combined with debug mode
-# is a big security no-no
-# Unless you have a firewall setup
-#
-# Something like /sbin/iptables -A INPUT -p tcp -i eth0 -s ! 71.236.239.43 --dport 5000 -j DROP
-# should do the trick
-#
-# You'll probably have to firewall the main port and the
-#
-# For more info see: http://www.cyberciti.biz/faq/iptables-block-port/
-
-#import logging
-#logging.basicConfig(filename="/tmp/flask_gn_log", level=logging.INFO)
-#
-#_log = logging.getLogger("search")
-#_ch = logging.StreamHandler()
-#_log.addHandler(_ch)
 
 import logging
 import utility.logger
