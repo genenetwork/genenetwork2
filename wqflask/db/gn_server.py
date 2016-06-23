4095e7d4d12cd4c06aa839a8c47ba38fbb9d30e3
Pjotr Prins <pjotr.public01@thebird.nl>
2016-06-23 10:27:47 +0000

    Added fetching menu info

---
+++ wqflask/db/gn_server.py
@@ -0,0 +1,9 @@
+# Backend query functions (logic)
+
+from db.call import gn_server
+
+from utility.logger import getLogger
+logger = getLogger(__name__ )
+
+def menu_main():
+    return gn_server("/int/menu/main.json")
