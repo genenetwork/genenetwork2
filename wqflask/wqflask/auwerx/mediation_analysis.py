48b8b931657c65e9b5f2a3ef0dcda3d5ad223f3a
Pjotr Prins <pjotr.public01@thebird.nl>
2017-03-05 12:44:17 +0000

    Unused imports

--- wqflask/wqflask/auwerx/mediation_analysis.py
+++ wqflask/wqflask/auwerx/mediation_analysis.py
@@ -1,14 +1,11 @@
 # PheWAS analysis for GN2
 # Author / Maintainer: Li Hao & Danny Arends <Danny.Arends@gmail.com>
+
 import sys
 from numpy import *
 import scipy as sp                            # SciPy
 import rpy2.robjects as ro                    # R Objects
 import rpy2.rinterface as ri
-
-from base.webqtlConfig import GENERATED_IMAGE_DIR
-from utility import webqtlUtil                # Random number for the image
-from utility import genofile_parser           # genofile_parser
 
 import base64
 import array
@@ -37,4 +34,3 @@ class Mediation(object):
         print("Processing Mediation output")
         template_vars = {}
         return(dict(template_vars))
-
