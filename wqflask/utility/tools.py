d30cbe103946d70b9c61bc48575f23ead1d4048c
Muriithi Frederick Muriuki <fredmanglis@gmail.com>
2018-01-19 13:00:51 +0300

    Update configurations
    * Have the authorisation URLs build up from the client id and client
      secret values.

--- wqflask/utility/tools.py
+++ wqflask/utility/tools.py
@@ -253,11 +253,11 @@ JS_GN_PATH         = get_setting('JS_GN_PATH')
 
 GITHUB_CLIENT_ID = get_setting('GITHUB_CLIENT_ID')
 GITHUB_CLIENT_SECRET = get_setting('GITHUB_CLIENT_SECRET')
-GITHUB_AUTH_URL = get_setting('GITHUB_AUTH_URL')
+GITHUB_AUTH_URL = "https://github.com/login/oauth/authorize?client_id="+GITHUB_CLIENT_ID+"&client_secret="+GITHUB_CLIENT_SECRET
 GITHUB_API_URL = get_setting('GITHUB_API_URL')
 ORCID_CLIENT_ID = get_setting('ORCID_CLIENT_ID')
 ORCID_CLIENT_SECRET = get_setting('ORCID_CLIENT_SECRET')
-ORCID_AUTH_URL = get_setting('ORCID_AUTH_URL')
+ORCID_AUTH_URL = "https://sandbox.orcid.org/oauth/authorize?response_type=code&scope=/authenticate&show_login=true&client_id="+ORCID_CLIENT_ID+"&client_secret="+ORCID_CLIENT_SECRET
 ORCID_TOKEN_URL = get_setting('ORCID_TOKEN_URL')
 
 ELASTICSEARCH_HOST = get_setting('ELASTICSEARCH_HOST')
