e147bc1ec354f9e07b7887109967afe8aae87f5b
Muriithi Frederick Muriuki <fredmanglis@gmail.com>
2018-03-09 18:15:19 +0300

    Add link_checker module
    * Add the module that will hold the code to test the links on the
      system.

---
+++ test/requests/link_checker.py
@@ -0,0 +1,9 @@
+import requests
+
+def check_links(args_obj, parser):
+    print("")
+    print("Checking links")
+    print("########################")
+    print("Not implemented yet.")
+    print("This is supposed to check all links in the system.")
+    print("########################")
