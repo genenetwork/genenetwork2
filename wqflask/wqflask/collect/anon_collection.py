424affe73c070be24991cbcbb4800e0d38f380b8
Muriithi Frederick Muriuki <fredmanglis@gmail.com>
2018-04-27 16:37:20 +0300

    Remove redis and simplify AnonCollection class
    * Remove the redis dependency for anonymous collections
    * Simplify the AnonCollection class since most of the persistence and
      retrieval code has moved into the utility functions.

--- wqflask/wqflask/collect/anon_collection.py
+++ wqflask/wqflask/collect/anon_collection.py
@@ -1,13 +1,12 @@
 import datetime
 import uuid
-import redis
 import simplejson as json
 from wqflask import user_manager
 from utility.logger import getLogger
 
-from .util_functions import process_traits
+from .util_functions import (get_collections_by_user_key, save_collection,
+                             process_traits)
 
-Redis = redis.StrictRedis()
 logger = getLogger(__name__)
 
 class AnonCollection(object):
@@ -17,104 +16,45 @@ class AnonCollection(object):
         self.key = anon_user.key
         self.name = collection_name
         self.id = None
+        self.traits = []
         self.created_timestamp = datetime.datetime.utcnow().strftime('%b %d %Y %I:%M%p')
-        self.changed_timestamp = self.created_timestamp #ZS: will be updated when changes are made
+        self.changed_timestamp = self.created_timestamp
 
-        #ZS: Find id and set it if the collection doesn't already exist
-        if Redis.get(self.key) == "None" or Redis.get(self.key) == None:
-            Redis.set(self.key, None) #ZS: For some reason I get the error "Operation against a key holding the wrong kind of value" if I don't do this
-        else:
-            collections_list = json.loads(Redis.get(self.key))
-            collection_position = 0 #ZS: Position of collection in collection_list, if it exists
-            collection_exists = False
-            for i, collection in enumerate(collections_list):
-                if collection['name'] == self.name:
-                    collection_position = i
-                    collection_exists = True
-                    self.id = collection['id']
-                    break
+        collections_list = get_collections_by_user_key(self.key)
+        logger.debug("Collections list", collections_list)
+        collection = [coll for coll in collections_list if coll["name"] == self.name]
+        if len(collection) == 1:
+            self.id = collection[0]['id']
+            self.traits = collection[0]['members']
+            self.is_new = False
 
         if self.id == None:
             self.id = str(uuid.uuid4())
+            self.is_new = True
 
     def get_members(self):
-        traits = []
-        collections_list = json.loads(Redis.get(self.key))
-        for collection in collections_list:
-            if collection['id'] == self.id:
-                traits = collection['members']
-        return traits
+        return self.traits
 
     @property
     def num_members(self):
-        num_members = 0
-        collections_list = json.loads(Redis.get(self.key))
-        for collection in collections_list:
-            if collection['id'] == self.id:
-                num_members = collection['num_members']
-        return num_members
+        return len(self.traits)
 
     def add_traits(self, params):
-        #assert collection_name == "Default", "Unexpected collection name for anonymous user"
-        self.traits = list(process_traits(params['traits']))
-        #len_before = len(Redis.smembers(self.key))
-        existing_collections = Redis.get(self.key)
-        logger.debug("existing_collections:", existing_collections)
-        if existing_collections != None and existing_collections != "None":
-            collections_list = json.loads(existing_collections)
-            collection_position = 0 #ZS: Position of collection in collection_list, if it exists
-            collection_exists = False
-            for i, collection in enumerate(collections_list):
-                if collection['id'] == self.id:
-                    collection_position = i
-                    collection_exists = True
-                    break
-            if collection_exists:
-                collections_list[collection_position]['members'].extend(self.traits)
-                collections_list[collection_position]['num_members'] = len(collections_list[collection_position]['members'])
-                collections_list[collection_position]['changed_timestamp'] = datetime.datetime.utcnow().strftime('%b %d %Y %I:%M%p')
-            else:
-                collection_dict = {"id" : self.id,
-                                   "name" : self.name,
-                                   "created_timestamp" : self.created_timestamp,
-                                   "changed_timestamp" : self.changed_timestamp,
-                                   "num_members" : len(self.traits),
-                                   "members" : self.traits}
-                collections_list.append(collection_dict)
-        else:
-            collections_list = []
-            collection_dict = {"id" : self.id,
-                               "name" : self.name,
-                               "created_timestamp" : self.created_timestamp,
-                               "changed_timestamp" : self.changed_timestamp,
-                               "num_members" : len(self.traits),
-                               "members" : self.traits}
-            collections_list.append(collection_dict)
-
-        Redis.set(self.key, json.dumps(collections_list))
-        #Redis.sadd(self.key, *list(traits))
-        #Redis.expire(self.key, 60 * 60 * 24 * 5)
-        #len_now = len(Redis.smembers(self.key))
-        #report_change(len_before, len_now)
+        self.traits.extend(list(process_traits(params['traits'])))
+        self.traits = list(set(self.traits))
 
     def remove_traits(self, params):
         traits_to_remove = [(":").join(trait.split(":")[:2]) for trait in params.getlist('traits[]')]
-        existing_collections = Redis.get(self.key)
-        collection_position = 0
-        collections_list = json.loads(existing_collections)
-        for i, collection in enumerate(collections_list):
-            if collection['id'] == self.id:
-                collection_position = i
-                collection_exists = True
-                break
-        collections_list[collection_position]['members'] = [trait for trait in collections_list[collection_position]['members'] if trait not in traits_to_remove]
-        collections_list[collection_position]['num_members'] = len(collections_list[collection_position]['members'])
-        collections_list[collection_position]['changed_timestamp'] = datetime.datetime.utcnow().strftime('%b %d %Y %I:%M%p')
-        len_now = collections_list[collection_position]['num_members']
-        #print("before in redis:", json.loads(Redis.get(self.key)))
-        Redis.set(self.key, json.dumps(collections_list))
-        #print("currently in redis:", json.loads(Redis.get(self.key)))
+        logger.debug("Traits to remove", traits_to_remove)
+        tmp = [trait for trait in self.traits if trait not in traits_to_remove]
+        self.traits = tmp
 
-        # We need to return something so we'll return this...maybe in the future
-        # we can use it to check the results
-        return str(len_now)
+    def get_data(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "created_timestamp": self.created_timestamp,
+            "changed_timestamp": self.changed_timestamp,
+            "num_members": len(self.traits),
+            "members": self.traits,
+            "user_key": self.key}
