3dfba31bc7b3dee8a32c52126eea2d22ccb16e47
Muriithi Frederick Muriuki <fredmanglis@gmail.com>
2018-04-27 16:35:15 +0300

    Add utility functions to handle collections
    * Add some utility functions to deal with saving and retrieving
      collections to and from elasticsearch

--- wqflask/wqflask/collect/util_functions.py
+++ wqflask/wqflask/collect/util_functions.py
@@ -1,6 +1,10 @@
 from wqflask import user_manager
-from utility.elasticsearch_tools import get_item_by_unique_column
+from utility.elasticsearch_tools import (get_elasticsearch_connection,
+                                         get_items_by_column, es_save_data,
+                                         es_update_data)
+from utility.logger import getLogger
 
+logger = getLogger(__name__)
 index = "collections"
 doc_type = "all"
 
@@ -8,6 +12,7 @@ def process_traits(unprocessed_traits):
     #print("unprocessed_traits are:", unprocessed_traits)
     if isinstance(unprocessed_traits, basestring):
         unprocessed_traits = unprocessed_traits.split(",")
+
     traits = set()
     for trait in unprocessed_traits:
         #print("trait is:", trait)
@@ -17,3 +22,33 @@ def process_traits(unprocessed_traits):
         traits.add(str(data))
 
     return traits
+
+def get_collections_by_user_key(key, doc_type="all"):
+    return get_items_by_column(
+        es = get_elasticsearch_connection(),
+        column_name = "user_key",
+        column_value = key,
+        index = "collections",
+        doc_type = doc_type)
+
+def get_collection_by_id(collection_id, doc_type="all"):
+    return get_item_by_unique_column(
+        es = get_elasticsearch_connection(),
+        column_name = "id",
+        column_value = collection_id,
+        index = "collections",
+        doc_type = doc_type)
+
+def save_collection(collection_id, collection):
+    if collection.is_new:
+        return es_save_data(
+            es = get_elasticsearch_connection(),
+            index = index, doc_type = doc_type,
+            data_item = collection.get_data(),
+            data_id = collection_id)
+    else:
+        return es_update_data(
+            es = get_elasticsearch_connection(),
+            index = index, doc_type = doc_type,
+            data_item = collection.get_data(),
+            data_id = collection_id)
