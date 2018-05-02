0659b777059c8181a80c783121bf4cbe89c5eb9c
Muriithi Frederick Muriuki <fredmanglis@gmail.com>
2018-05-02 07:04:33 +0300

    Add functions to deal with cookies
    * Add functions to save, retrieve and delete cookies to enable user to
      remain logged in to the system until the cookie expires.

--- wqflask/wqflask/user_manager/util_functions.py
+++ wqflask/wqflask/user_manager/util_functions.py
@@ -3,9 +3,14 @@ import hashlib
 import datetime
 import hmac
 from wqflask import app
+from flask import request
 from utility.logger import getLogger
+from utility.elasticsearch_tools import (es_save_data, get_item_by_unique_column,
+                                         get_elasticsearch_connection, es_delete_data_by_id)
 
 logger = getLogger(__name__)
+
+cookie_name = 'session_id_v2'
 
 def timestamp():
     return datetime.datetime.utcnow().isoformat()
@@ -35,3 +40,25 @@ def actual_hmac_creation(stringy):
     # http://www.w3.org/QA/2009/07/hmac_truncation_in_xml_signatu.html
     hm = hm[:20]
     return hm
+
+def save_cookie_details(cookie_id, user):
+    es_save_data(
+        es = get_elasticsearch_connection(),
+        index = "cookies",
+        doc_type = "local",
+        data_item = {
+            "cookie_id": cookie_id,
+            "user": user
+        },
+        data_id = cookie_id)
+
+def get_cookie_details(cookie_id):
+    return get_item_by_unique_column(
+        es = get_elasticsearch_connection(), column_name = "cookie_id",
+        column_value = cookie_id, index = "cookies", doc_type = "local")
+
+def delete_cookie_details():
+    cookie_id = request.cookies.get(cookie_name)
+    return es_delete_data_by_id(
+        es = get_elasticsearch_connection(), index = "cookies",
+        doc_type = "local", data_id=cookie_id)
