ee8e1eacd88399609f530a33e2d91cf7895da5b0
zsloan <zachary.a.sloan@gmail.com>
2018-08-15 20:25:28 +0000

    The issue with adding anonymous collections should be fixed now

--- wqflask/wqflask/user_manager.py
+++ wqflask/wqflask/user_manager.py
@@ -56,12 +56,10 @@ class AnonUser(object):
         if self.cookie:
             logger.debug("ANON COOKIE ALREADY EXISTS")
             self.anon_id = verify_cookie(self.cookie)
-
         else:
             logger.debug("CREATING NEW ANON COOKIE")
             self.anon_id, self.cookie = create_signed_cookie()
-            res = make_response()
-            res.set_cookie(self.cookie_name, self.cookie)
+
         self.key = "anon_collection:v1:{}".format(self.anon_id)
 
     def add_collection(self, new_collection):
@@ -131,6 +129,7 @@ class AnonUser(object):
         except Exception as why:
             print("Couldn't display_num_collections:", why)
             return ""
+
 
 def verify_cookie(cookie):
     the_uuid, separator, the_signature = cookie.partition(':')
@@ -412,11 +411,16 @@ def before_request():
     g.user_session = UserSession()
     g.cookie_session = AnonUser()
 
+@app.after_request
+def set_cookie(response):
+    if not request.cookies.get(g.cookie_session.cookie_name):
+        response.set_cookie(g.cookie_session.cookie_name, g.cookie_session.cookie)
+    return response
+
 class UsersManager(object):
     def __init__(self):
         self.users = model.User.query.all()
         logger.debug("Users are:", self.users)
-
 
 class UserManager(object):
     def __init__(self, kw):
