e3c0745fdc3bf33cb10256643f3b1469ee6614c4
Muriithi Frederick Muriuki <fredmanglis@gmail.com>
2019-03-09 08:33:19 +0300

    Add draw_open_polygon() utility
    * wqflask/utility/pillow_utils.py: New method.

--- wqflask/utility/pillow_utils.py
+++ wqflask/utility/pillow_utils.py
@@ -4,6 +4,8 @@ import utility.logger
 logger = utility.logger.getLogger(__name__ )
 
 BLACK = ImageColor.getrgb("black")
+WHITE = ImageColor.getrgb("white")
+
 # def draw_rotated_text(canvas: Image, text: str, font: ImageFont, xy: tuple, fill: ImageColor=BLACK, angle: int=-90):
 def draw_rotated_text(canvas, text, font, xy, fill=BLACK, angle=-90):
     # type: (Image, str, ImageFont, tuple, ImageColor, int)
@@ -14,3 +16,10 @@ def draw_rotated_text(canvas, text, font, xy, fill=BLACK, angle=-90):
     tmp_img2 = tmp_img.rotate(angle, expand=1)
     tmp_img2.save("/tmp/{}.png".format(text), format="png")
     canvas.paste(im=tmp_img2, box=tuple([int(i) for i in xy]))
+
+# def draw_open_polygon(canvas: Image, xy: tuple, fill: ImageColor=WHITE, outline: ImageColor=BLACK):
+def draw_open_polygon(canvas, xy, fill=None, outline=BLACK, width=0):
+    # type: (Image, tuple, ImageColor, ImageColor)
+    draw_ctx = ImageDraw.Draw(canvas)
+    draw_ctx.polygon(xy, fill=fill)
+    draw_ctx.line(xy, fill=outline, width=width)
