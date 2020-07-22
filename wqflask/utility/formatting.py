073e8c9813505fc00372b2ec21f3e4a87b5be9aa
BonfaceKilz <bonfacemunyoki@gmail.com>
2020-07-22 17:05:11 +0300

    Apply autopep-8
    * wqflask/utility/formatting.py: apply it

--- wqflask/utility/formatting.py
+++ wqflask/utility/formatting.py
@@ -28,22 +28,20 @@ def numify(number, singular=None, plural=None):
     '12,334 hippopotami'
 
     """
-    num_repr = {0 : "zero",
-                1 : "one",
-                2 : "two",
-                3 : "three",
-                4 : "four",
-                5 : "five",
-                6 : "six",
-                7 : "seven",
-                8 : "eight",
-                9 : "nine",
-                10 : "ten",
-                11 : "eleven",
-                12 : "twelve"}
+    num_repr = {0: "zero",
+                1: "one",
+                2: "two",
+                3: "three",
+                4: "four",
+                5: "five",
+                6: "six",
+                7: "seven",
+                8: "eight",
+                9: "nine",
+                10: "ten",
+                11: "eleven",
+                12: "twelve"}
 
-    #Below line commented out cause doesn't work in Python 2.4
-    #assert all((singular, plural)) or not any((singular, plural)), "Need to pass two words or none"
     if number == 1:
         word = singular
     else:
