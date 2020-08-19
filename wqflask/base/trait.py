1c3ade2f739608563cc1659dd93e5b2abc446046
BonfaceKilz <bonfacemunyoki@gmail.com>
2020-08-19 03:27:21 +0300

    Replace backtick repr with the repr() function
    * wqflask/base/trait.py: See
    <https://docs.python.org/2/library/2to3.html#2to3fixer-repr>

--- wqflask/base/trait.py
+++ wqflask/base/trait.py
@@ -605,6 +605,6 @@ def retrieve_trait_info(trait, dataset, get_qtl_info=False):
                 if trait.lrs != "":
                     trait.LRS_score_repr = LRS_score_repr = '%3.1f' % trait.lrs
     else:
-        raise KeyError(`trait.name`+' information is not found in the database.')
+        raise KeyError(repr(trait.name)+' information is not found in the database.')
         
     return trait
