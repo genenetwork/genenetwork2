657ab53bef4fc767fdc549f004153a7910b45bd0
zsloan <zachary.a.sloan@gmail.com>
2020-09-10 14:33:52 -0500

    Fixed several sorting issues and moved orderDataType definitions into search_results.js since all the tables that use them import that file (that file's name needs to be changed, though) * wqflask/wqflask/static/new/javascript/search_results.js - Added the dom-checkbox and dom-inner-text order types into this file; they were previously duplicated across several template files * wqflask/wqflask/templates/gsearch_gene.html - Added dom-inner-text order type to relevant columns with hyperlink contents and disabled deferRender since it conflicted with use of both pagination and orderDataType for sorting * wqflask/wqflask/templates/gsearch_pheno.html - Same as above * wqflask/wqflask/templates/mapping_results.html - Added dom-inner-text for a few columns that have hyperlink values - still need to modify natural-minus-na to also include other "N/A"-equivalent values like "--" * wqflask/wqflask/templates/search_result_page.html Removed the dom-checkbox and dom-inner-text order types from this file since they were moved to search_results.js

--- wqflask/wqflask/static/new/javascript/search_results.js
+++ wqflask/wqflask/static/new/javascript/search_results.js
@@ -281,4 +281,18 @@ $(function() {
     }
   });
 
+  $.fn.dataTable.ext.order['dom-checkbox'] = function  ( settings, col )
+  {
+      return this.api().column( col, {order:'index'} ).nodes().map( function ( td, i ) {
+          return $('input', td).prop('checked') ? '1' : '0';
+      } );
+  };
+
+  $.fn.dataTable.ext.order['dom-inner-text'] = function  ( settings, col )
+  {
+      return this.api().column( col, {order:'index'} ).nodes().map( function ( td, i ) {
+          return $(td).text();
+      } );
+  }
+
 });
