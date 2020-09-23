d9853ade5a70843c47792e8327301b7b3d5d4a6b
BonfaceKilz <bonfacemunyoki@gmail.com>
2020-09-23 03:01:35 +0300

    Fix failing builds
    * wqflask/wqflask/__init__.py: Move imports from views.py to
    "__init__.py". Fixes failing build.
    * wqflask/wqflask/views.py: Ditto.

--- wqflask/wqflask/__init__.py
+++ wqflask/wqflask/__init__.py
@@ -12,13 +12,24 @@ logging.basicConfig(level=logging.INFO)
 
 app = Flask(__name__)
 
-app.config.from_envvar('GN2_SETTINGS')       # See http://flask.pocoo.org/docs/config/#configuring-from-files
+# See http://flask.pocoo.org/docs/config/#configuring-from-files
 # Note no longer use the badly named WQFLASK_OVERRIDES (nyi)
-
+app.config.from_envvar('GN2_SETTINGS')
 app.jinja_env.globals.update(
-    undefined = jinja2.StrictUndefined,
-    numify = formatting.numify
-)
+    undefined=jinja2.StrictUndefined,
+    numify=formatting.numify)
 
 from wqflask.api import router
+from wqflask import group_manager
+from wqflask import resource_manager
+from wqflask import search_results
+from wqflask import export_traits
+from wqflask import gsearch
+from wqflask import update_search_results
+from wqflask import docs
+from wqflask import news
+from wqflask import db_info
+from wqflask import user_login
+from wqflask import user_session
+
 import wqflask.views
