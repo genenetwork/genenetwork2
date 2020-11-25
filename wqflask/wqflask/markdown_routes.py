23d27c88bcc085bac3fb14b534cf1d4a7f11f22a
Alexander Kabui <alexanderkabua@gmail.com>
2020-11-26 00:07:01 +0300

    modify references links

--- wqflask/wqflask/markdown_routes.py
+++ wqflask/wqflask/markdown_routes.py
@@ -9,7 +9,10 @@ from flask import Blueprint
 from flask import render_template
 
 glossary_blueprint = Blueprint('glossary_blueprint', __name__)
-
+references_blueprint=Blueprint("references_blueprint",__name__)
+environments_blueprint=Blueprint("environments_blueprint",__name__)
+links_blueprint=Blueprint("links_blueprint",__name__)
+policies_blueprint=Blueprint("policies_blueprint",__name__)
 
 def render_markdown(file_name):
     """Try to fetch the file name from Github and if that fails, try to
@@ -22,6 +25,7 @@ look for it inside the file system
     if md_content.status_code == 200:
         return markdown.Markdown().convert(md_content.content.decode("utf-8"))
 
+
     # TODO: Add fallback on our git server by checking the mirror.
 
     # Content not available
@@ -32,8 +36,35 @@ look for it inside the file system
             "Please reach out to the gn2 team to have a look at this")
 
 
+
+
 @glossary_blueprint.route('/')
 def glossary():
     return render_template(
         "glossary.html",
         rendered_markdown=render_markdown("general/glossary/glossary.md")), 200
+
+
+@references_blueprint.route('/')
+def references():
+    return render_template(
+        "reference.html",
+        rendered_markdown=render_markdown("general/glossary/glossary.md")), 200
+
+
+
+@environments_blueprint.route("/")
+def references():
+    return render_template("environments.html",rendered_markdown=render_markdown("general/environments/environments.md"))
+
+
+@links_blueprint.route("/")
+def links():
+    return rendered_template("links.html",rendered_markdown=render_markdown("general/links/links.md"))
+
+
+policies_blueprint.route("/")
+def policies():
+    return rendered_template("policies.html",rendered_markdown=rendered_markdown("general/policies/policies.md"))
+
+
