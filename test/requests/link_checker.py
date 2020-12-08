0570385d97b3372debb54e943b0632df82a1dfeb
Alexander Kabui <alexanderkabua@gmail.com>
2020-12-08 14:07:45 +0300

    add  jquery-cookie to mechanical rob

--- test/requests/link_checker.py
+++ test/requests/link_checker.py
@@ -117,6 +117,7 @@ def check_packaged_js_files(args_obj, parser):
         "/js/qtip2/jquery.qtip.min.js",
         "/js/js_alt/md5.min.js",
         "/js/jquery-ui/jquery-ui.min.js",
+        "/js/jquery-cookie/jquery.cookie.js",
     ]
 
     print("Checking links")
