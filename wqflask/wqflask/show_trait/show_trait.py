6c63b869d047daca44ff5facee44833f52b0d861
zsloan <zachary.a.sloan@gmail.com>
2021-01-11 13:49:04 -0600

    Added something to line accounting for certain BXD individual groups when building the correlation drop-down

--- wqflask/wqflask/show_trait/show_trait.py
+++ wqflask/wqflask/show_trait/show_trait.py
@@ -372,7 +372,7 @@ class ShowTrait(object):
 
         # We're checking a string here!
         assert isinstance(this_group, str), "We need a string type thing here"
-        if this_group[:3] == 'BXD' and this_group != "BXD-Longevity":
+        if this_group[:3] == 'BXD' and this_group != "BXD-Longevity" and this_group != "BXD-AE":
             this_group = 'BXD'
 
         if this_group:
