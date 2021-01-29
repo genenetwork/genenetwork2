5c8031be39fb25c5aa39938559cb728e57ef1204
BonfaceKilz <me@bonfacemunyoki.com>
2021-01-29 10:33:15 +0300

    Add function that calculates the hash of a folder
    * wqflask/wqflask/api/router.py (get_hash_of_dirs): New function.

--- wqflask/wqflask/api/router.py
+++ wqflask/wqflask/api/router.py
@@ -7,6 +7,7 @@ import json
 import datetime
 import requests
 
+import hashlib
 from zipfile import ZipFile, ZIP_DEFLATED
 
 
@@ -26,6 +27,41 @@ import utility.logger
 logger = utility.logger.getLogger(__name__ )
 
 version = "pre1"
+
+
+def get_hash_of_dirs(directory, verbose=0):
+    SHAhash = hashlib.md5()
+    if not os.path.exists(directory):
+        return -1
+
+    try:
+        for root, dirs, files in os.walk(directory):
+            for names in files:
+                if verbose == 1:
+                    filepath = os.path.join(root, names)
+            try:
+                f1 = open(filepath, 'rb')
+            except:
+                # You can't open the file for some reason
+                f1.close()
+                continue
+
+            while 1:
+                # Read file in as little chunks
+                buf = f1.read(4096)
+                if not buf:
+                    break
+                SHAhash.update(hashlib.md5(buf).hexdigest())
+                f1.close()
+
+    except:
+        import traceback
+        # Print the stack traceback
+        traceback.print_exc()
+        return -2
+
+    return SHAhash.hexdigest()
+
 
 @app.route("/api/v_{}/".format(version))
 def hello_world():
