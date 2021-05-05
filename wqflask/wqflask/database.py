19de25008ca2513a9fca00199bba52b55a42f11b
BonfaceKilz <me@bonfacemunyoki.com>
2021-05-05 16:26:09 +0300

    Delete model.py and it's references
    * wqflask/wqflask/database.py (init_db): Remove references to
    "wqflask.model"
    * wqflask/wqflask/model.py: Delete it.

--- wqflask/wqflask/database.py
+++ wqflask/wqflask/database.py
@@ -16,11 +16,6 @@ Base.query = db_session.query_property()
 
 
 def init_db():
-    # import all modules here that might define models so that
-    # they will be registered properly on the metadata.  Otherwise
-    # you will have to import them first before calling init_db()
-    #import yourapplication.models
-    import wqflask.model
     Base.metadata.create_all(bind=engine)
 
 
