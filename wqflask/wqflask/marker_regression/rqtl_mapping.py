2444d60a93ef7c9900ed9a52877bff0ee08fbfb6
zsloan <zachary.a.sloan@gmail.com>
2021-05-06 22:54:05 +0000

    Because each permutations value is returned as an array, need to just take the first item to convert to numbers

--- wqflask/wqflask/marker_regression/rqtl_mapping.py
+++ wqflask/wqflask/marker_regression/rqtl_mapping.py
@@ -385,7 +385,7 @@ def process_pair_scan_results(result):
 def process_rqtl_perm_results(num_perm, results):
     perm_vals = []
     for item in results:
-        perm_vals.append(item)
+        perm_vals.append(item[0])
 
     perm_output = perm_vals
     suggestive = np.percentile(np.array(perm_vals), 67)
