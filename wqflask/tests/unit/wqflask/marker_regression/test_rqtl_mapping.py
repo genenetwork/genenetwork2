09768ed9c255cdd10561eeba28bf3752747bf378
zsloan <zachary.a.sloan@gmail.com>
2021-05-27 20:29:57 +0000

    Removed parts of test_rqtl_mapping.py referring to process_perm_results and process_rqtl_results since that functionality was moved to GN3

--- wqflask/tests/unit/wqflask/marker_regression/test_rqtl_mapping.py
+++ wqflask/tests/unit/wqflask/marker_regression/test_rqtl_mapping.py
@@ -13,12 +13,10 @@ class MockDataset:
 
 class TestRqtlMapping(unittest.TestCase):
     """Tests for functions in rqtl_mapping.py"""
-    @mock.patch("wqflask.marker_regression.rqtl_mapping.process_rqtl_results")
-    @mock.patch("wqflask.marker_regression.rqtl_mapping.process_perm_results")
     @mock.patch("wqflask.marker_regression.rqtl_mapping.requests.post")
     @mock.patch("wqflask.marker_regression.rqtl_mapping.locate")
     @mock.patch("wqflask.marker_regression.rqtl_mapping.write_phenotype_file")
-    def test_run_rqtl_with_perm(self, mock_write_pheno_file, mock_locate, mock_post, mock_process_perm, mock_process_rqtl):
+    def test_run_rqtl_with_perm(self, mock_write_pheno_file, mock_locate, mock_post):
         """Test for run_rqtl with permutations > 0"""
         dataset_group = MockGroup(
             {"name": "GP1", "genofile": "file_geno"})
@@ -28,8 +26,6 @@ class TestRqtlMapping(unittest.TestCase):
         mock_locate.return_value = "geno_filename"
         mock_post.return_value = {"output_file": "output_filename",
                                   "rqtl_cmd": "the_command"}
-        mock_process_perm.return_value = [[], 3, 4]
-        mock_process_rqtl.return_value = []
 
         results = run_rqtl(trait_name="the_trait", vals=[], samples=[],
         dataset=dataset, mapping_scale="cM", model="normal", method="hk",
@@ -39,6 +35,5 @@ class TestRqtlMapping(unittest.TestCase):
         mock_write_pheno_file.assert_called_once()
         mock_locate.assert_called_once()
         mock_post.assert_called_once()
-        mock_process_perm.assert_called_once()
-        mock_process_rqtl.assert_called_once()
+
         self.assertEqual(results, ([], 3, 4, []))
