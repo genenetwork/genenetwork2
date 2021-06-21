ef49a5bf5b1b2b010c71f77394c5b178c00b8942
zsloan <zachary.a.sloan@gmail.com>
2021-06-21 20:29:26 +0000

    Fixed indentation in table_functions.js

--- wqflask/wqflask/static/new/javascript/table_functions.js
+++ wqflask/wqflask/static/new/javascript/table_functions.js
@@ -1,72 +1,69 @@
 recheck_rows = function(the_table, checked_rows){
-    //ZS: This is meant to recheck checkboxes after columns are resized
-    check_cells = the_table.column(0).nodes().to$();
-    for (let i = 0; i < check_cells.length; i++) {
-      if (checked_rows.includes(i)){
-        check_cells[i].childNodes[0].checked = true;
-      }
-    }
-
-    check_rows = trait_table.rows().nodes();
-    for (let i =0; i < check_rows.length; i++) {
-      if (checked_rows.includes(i)){
-        check_rows[i].classList.add("selected")
-      }
+  //ZS: This is meant to recheck checkboxes after columns are resized
+  check_cells = the_table.column(0).nodes().to$();
+  for (let i = 0; i < check_cells.length; i++) {
+    if (checked_rows.includes(i)){
+      check_cells[i].childNodes[0].checked = true;
     }
   }
 
-get_checked_rows = function(table_id){
-    let checked_rows = []
-    $("#" + table_id + " input").each(function(index){
-      if ($(this).prop("checked") == true){
-        checked_rows.push(index);
-      }
-    });
+  check_rows = trait_table.rows().nodes();
+  for (let i =0; i < check_rows.length; i++) {
+    if (checked_rows.includes(i)){
+      check_rows[i].classList.add("selected")
+    }
+  }
+}
 
-    return checked_rows
+get_checked_rows = function(table_id){
+  let checked_rows = []
+  $("#" + table_id + " input").each(function(index){
+    if ($(this).prop("checked") == true){
+      checked_rows.push(index);
+    }
+  });
+
+  return checked_rows
 }
 
 function setUserColumnsDefWidths(table_id) {
-    var userColumnDef;
+  var userColumnDef;
 
-    // Get the settings for this table from localStorage
-    var userColumnDefs = JSON.parse(localStorage.getItem(table_id)) || [];
+  // Get the settings for this table from localStorage
+  var userColumnDefs = JSON.parse(localStorage.getItem(table_id)) || [];
 
-    if (userColumnDefs.length === 0 ) return;
+  if (userColumnDefs.length === 0 ) return;
 
-    columnDefs.forEach( function(columnDef) {
-
-      // Check if there is a width specified for this column
-      userColumnDef = userColumnDefs.find( function(column) {
-        return column.targets === columnDef.targets;
-      });
-
-      // If there is, set the width of this columnDef in px
-      if ( userColumnDef ) {
-
-        columnDef.sWidth = userColumnDef.width + 'px';
-        columnDef.width = userColumnDef.width + 'px';
-
-        $('.toggle-vis').each(function(){
-          if ($(this).attr('data-column') == columnDef.targets){
-            if ($(this).hasClass("active")){
-              columnDef.bVisible = false
-            } else {
-              columnDef.bVisible = true
-            }
-          }
-        })
-      }
+  columnDefs.forEach( function(columnDef) {
+    // Check if there is a width specified for this column
+    userColumnDef = userColumnDefs.find( function(column) {
+      return column.targets === columnDef.targets;
     });
+
+    // If there is, set the width of this columnDef in px
+    if ( userColumnDef ) {
+
+      columnDef.sWidth = userColumnDef.width + 'px';
+      columnDef.width = userColumnDef.width + 'px';
+
+      $('.toggle-vis').each(function(){
+        if ($(this).attr('data-column') == columnDef.targets){
+          if ($(this).hasClass("active")){
+            columnDef.bVisible = false
+          } else {
+            columnDef.bVisible = true
+          }
+        }
+      })
+    }
+  });
 }
 
 function saveColumnSettings(table_id, trait_table) {
-var userColumnDefs = JSON.parse(localStorage.getItem(table_id)) || [];
+  var userColumnDefs = JSON.parse(localStorage.getItem(table_id)) || [];
+  var width, header, existingSetting; 
 
-var width, header, existingSetting; 
-
-trait_table.columns().every( function ( targets ) {
-
+  trait_table.columns().every( function ( targets ) {
     // Check if there is a setting for this column in localStorage
     existingSetting = userColumnDefs.findIndex( function(column) { return column.targets === targets;});
 
@@ -78,16 +75,14 @@ trait_table.columns().every( function ( targets ) {
     // Update the width
     userColumnDefs[existingSetting].width = width;
     } else {
-
-    // Add the width for this column
-    userColumnDefs.push({
-        targets: targets,
-        width:  width,
-    });
-
+      // Add the width for this column
+      userColumnDefs.push({
+          targets: targets,
+          width:  width,
+      });
     }
-});
+  });
 
-// Save (or update) the settings in localStorage
-localStorage.setItem(table_id, JSON.stringify(userColumnDefs));
+  // Save (or update) the settings in localStorage
+  localStorage.setItem(table_id, JSON.stringify(userColumnDefs));
 }
