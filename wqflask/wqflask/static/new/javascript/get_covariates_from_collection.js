367e6c2d49284942e1ddfc3da192cffaee2a78f3
zsloan <zachary.a.sloan@gmail.com>
2021-08-17 20:20:15 +0000

    Removed ID of 'submit' from the button in the cofactor select table, since it was causing trait page forms to not submit after opening it

--- wqflask/wqflask/static/new/javascript/get_covariates_from_collection.js
+++ wqflask/wqflask/static/new/javascript/get_covariates_from_collection.js
@@ -211,7 +211,7 @@ process_traits = function(trait_data, textStatus, jqXHR) {
   console.log('in process_traits with trait_data:', trait_data);
   the_html = "<button id='back_to_collections' class='btn btn-inverse btn-small'>";
   the_html += "<i class='icon-white icon-arrow-left'></i> Back </button>";
-  the_html += "    <button id='submit' class='btn btn-primary btn-small'> Submit </button>";
+  the_html += "    <button class='btn btn-primary btn-small submit'> Submit </button>";
   the_html += "<table id='collection_table' style='padding-top: 10px;' class='table table-hover'>";
   the_html += "<thead><tr><th></th><th>Record</th><th>Data Set</th><th>Description</th></tr></thead>";
   the_html += "<tbody>";
@@ -244,6 +244,6 @@ back_to_collections = function() {
 };
 
 $(".collection_line").on("click", collection_click);
-$("#submit").on("click", submit_click);
+$(".submit").on("click", submit_click);
 $(".trait").on("click", trait_click);
 $("#back_to_collections").on("click", back_to_collections);
