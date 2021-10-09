c9ae69a30a972f47232f8457e9e1b8cd514f9832
Alexander Kabui <alexanderkabua@gmail.com>
2021-10-09 22:04:17 +0300

    add function to process trait sample data

--- wqflask/wqflask/wgcna/gn3_wgcna.py
+++ wqflask/wqflask/wgcna/gn3_wgcna.py
@@ -1,6 +1,7 @@
 """module contains code to consume gn3-wgcna api
 and process data to be rendered by datatables
 """
+from utility.helper_functions import get_trait_db_obs
 
 
 def fetch_trait_data(requestform):
@@ -11,6 +12,49 @@ def fetch_trait_data(requestform):
                       for trait in requestform['trait_list'].split(',')])
 
     return process_dataset(db_obj.trait_list)
+
+
+def process_dataset(trait_list):
+    """process datasets and strains"""
+
+    input_data = {}
+    traits = []
+    strains = []
+
+    # xtodo unique traits and strains
+
+    for trait in trait_list:
+        traits.append(trait[0].name)
+
+        input_data[trait[0].name] = {}
+        for strain in trait[0].data:
+            strains.append(strain)
+            input_data[trait[0].name][strain] = trait[0].data[strain].value
+
+    return {
+        "wgcna_input": input_data
+    }
+
+    def process_dataset(trait_list):
+    """process datasets and strains"""
+
+    input_data = {}
+    traits = []
+    strains = []
+
+    # xtodo unique traits and strains
+
+    for trait in trait_list:
+        traits.append(trait[0].name)
+
+        input_data[trait[0].name] = {}
+        for strain in trait[0].data:
+            strains.append(strain)
+            input_data[trait[0].name][strain] = trait[0].data[strain].value
+
+    return {
+        "wgcna_input": input_data
+    }
 
 
 def process_wgcna_data(response):
