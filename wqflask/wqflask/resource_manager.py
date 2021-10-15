6195ad475cddce2007b125b856829cbb41defd30
BonfaceKilz <me@bonfacemunyoki.com>
2021-10-15 08:49:23 +0300

    resource_manager: Add OrderedEnum
    Borrowed from:
    <https://github.com/woodruffw/ordered_enum/tree/master/src/ordered_enum>

--- wqflask/wqflask/resource_manager.py
+++ wqflask/wqflask/resource_manager.py
@@ -1,13 +1,26 @@
 import redis
 import json
+import functools
 
+from enum import Enum
 
 
 
 from typing import Dict
 
 
+@functools.total_ordering
+class OrderedEnum(Enum):
+    @classmethod
+    @functools.lru_cache(None)
+    def _member_list(cls):
+        return list(cls)
 
+    def __lt__(self, other):
+        if self.__class__ is other.__class__:
+            member_list = self.__class__._member_list()
+            return member_list.index(self) < member_list.index(other)
+        return NotImplemented
 
 
 
