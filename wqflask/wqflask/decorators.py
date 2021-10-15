35adb37736cf661578b5089b72f6c694554ba1b3
BonfaceKilz <me@bonfacemunyoki.com>
2021-10-15 15:47:21 +0300

    Remove "utility.hmac.hmac_creation" which causes circular imports
    Hacky but re-implement `hmac_creation` as `create_hmac`

--- wqflask/wqflask/decorators.py
+++ wqflask/wqflask/decorators.py
@@ -1,26 +1,34 @@
 """This module contains gn2 decorators"""
-from flask import g
+import hashlib
+import hmac
+from flask import current_app, g
 from typing import Dict
 from functools import wraps
-from utility.hmac import hmac_creation
-from utility.tools import GN_PROXY_URL
 
 import json
 import requests
 
 
+def create_hmac(data: str, secret: str) -> str:
+    return hmac.new(bytearray(secret, "latin-1"),
+                    bytearray(data, "utf-8"),
+                    hashlib.sha1).hexdigest[:20]
 def edit_access_required(f):
     """Use this for endpoints where admins are required"""
     @wraps(f)
     def wrap(*args, **kwargs):
         resource_id: str = ""
         if kwargs.get("inbredset_id"):  # data type: dataset-publish
-            resource_id = hmac_creation("dataset-publish:"
-                                        f"{kwargs.get('inbredset_id')}:"
-                                        f"{kwargs.get('name')}")
+            resource_id = create_hmac(
+                data=("dataset-publish:"
+                      f"{kwargs.get('inbredset_id')}:"
+                      f"{kwargs.get('name')}"),
+                secret=current_app.config.get("SECRET_HMAC_CODE"))
         if kwargs.get("dataset_name"):  # data type: dataset-probe
-            resource_id = hmac_creation("dataset-probeset:"
-                                        f"{kwargs.get('dataset_name')}")
+            resource_id = create_hmac(
+                data=("dataset-probeset:"
+                      f"{kwargs.get('dataset_name')}"),
+                secret=current_app.config.get("SECRET_HMAC_CODE"))
         response: Dict = {}
         try:
             _user_id = g.user_session.record.get(b"user_id",
