cb4957926688a0f2aeedebc17e52ffe835f259af
Alexander Kabui <alexanderkabua@gmail.com>
2022-01-19 12:58:10 +0300

    parse input data

--- wqflask/wqflask/ctl/gn3_ctl_analysis.py
+++ wqflask/wqflask/ctl/gn3_ctl_analysis.py
@@ -3,6 +3,20 @@ import requests
 from utility.tools import GN3_LOCAL_URL
 
 
+def parse_form_data(form_data: dict):
+    """function to parse/validate form data
+    input: dict containing required data
+    output: dict with parsed data
+
+    """
+
+    form_data["nperm"] = int(form_data["nperm"])
+    form_data["significance"] = float(int(form_data["significance"]))
+    form_data["strategy"] = form_data["strategy"].capitalize()
+
+    return form_data
+
+
 def run_ctl():
     """function to make an api call
     to gn3 and run ctl"""
