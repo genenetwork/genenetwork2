6683c13d838de422e2bbc7693907085dee45827d
jgart <jgart@dismail.de>
2022-02-05 02:16:45 -0500

    Update live jupyter notebooks

--- wqflask/wqflask/jupyter_notebooks.py
+++ wqflask/wqflask/jupyter_notebooks.py
@@ -7,17 +7,22 @@ jupyter_notebooks = Blueprint("jupyter_notebooks", __name__)
 def launcher():
     links = (
         {
-            "main_url": "http://notebook.genenetwork.org/40771/notebooks/2020-05-08/solberg-rat-analysis.ipynb",
+            "main_url": "http://notebook.genenetwork.org/58965/notebooks/2020-05-08/solberg-rat-analysis.ipynb",
             "notebook_name": "Quantitative Genetics Tools for Mapping Trait Variation to Mechanisms, Therapeutics, and Interventions - Webinar Series",
             "src_link_url": "https://github.com/senresearch/quant-genetics-webinars",
         },
         {
-            "main_url": "http://notebook.genenetwork.org/45837/notebooks/genenetwork.ipynb",
+            "main_url": "http://notebook.genenetwork.org/58163/notebooks/BXD%20Analysis.ipynb",
+            "notebook_name": "This shows how to model BXD mouse weight data using an AR(1) process.",
+            "src_link_url": "https://github.com/BonfaceKilz/tsaf-analysis-of-bxd-mouse-colonies",
+        },
+        {
+            "main_url": "http://notebook.genenetwork.org/46649/notebooks/genenetwork.ipynb",
             "notebook_name": "Querying the GeneNetwork API declaratively with python.",
             "src_link_url": "https://github.com/jgarte/genenetwork-jupyter-notebook-example",
         },
         {
-            "main_url": "http://notebook.genenetwork.org/38079/notebooks/genenetwork-api-using-r.ipynb",
+            "main_url": "http://notebook.genenetwork.org/37279/notebooks/genenetwork-api-using-r.ipynb",
             "notebook_name": "R notebook showing how to query the GeneNetwork API.",
             "src_link_url": "https://github.com/jgarte/genenetwork-api-r-jupyter-notebook",
         },
