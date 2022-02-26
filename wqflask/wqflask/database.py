80b02d37f60d172be01bf8cd62bd84b406b1e0dd
Frederick Muriuki Muriithi <fredmanglis@gmail.com>
2022-02-26 08:11:09 +0300

    Use one-step process for the partial correlations computations
    This commit gets rid of the multi-step partial correlations process
    replacing it with a single-step process.
    
    Summary of changes:
    * wqflask/wqflask/collect.py: Add function to format the trait details
      in a format that is usable for the partial correlations system.
    * wqflask/wqflask/database.py: Provide function to create a connection
      to the database
    * wqflask/wqflask/partial_correlations_views.py: Rework the code to
      enable the one-step process for the partial correlations
      computations
    * wqflask/wqflask/static/new/javascript/partial_correlations.js: Get
      rid of code that supported the multi-step process
    * wqflask/wqflask/templates/collections/view.html: Remove inconsistent
      UI elements. Attach traits info in a form usable for the partial
      correlations
    * wqflask/wqflask/templates/partial_correlations.html: delete html
      template
    * wqflask/wqflask/templates/partial_correlations/pcorrs_error.html:
      provide a html template to display errors in the partial
      correlations computation process
    * wqflask/wqflask/templates/partial_correlations/pcorrs_poll_results.html:
      UI template to provide user with feedback as the computations
      continue in the background
    * wqflask/wqflask/templates/partial_correlations/pcorrs_results_presentation.html:
      UI template to present the results of successful computation
    * wqflask/wqflask/templates/partial_correlations/pcorrs_select_operations.html:
      UI template to trigger the partial correlations computations
    * wqflask/wqflask/templates/tool_buttons.html: Add the partial
      correlations button to the template to ensure a consistent look and
      feel

--- wqflask/wqflask/database.py
+++ wqflask/wqflask/database.py
@@ -1,9 +1,12 @@
 # Module to initialize sqlalchemy with flask
+import MySQLdb
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from utility.tools import SQL_URI
+
+from flask import current_app
 
 
 engine = create_engine(SQL_URI, encoding="latin1")
@@ -16,3 +19,12 @@ Base.query = db_session.query_property()
 
 # Initialise the db
 Base.metadata.create_all(bind=engine)
+
+
+def database_connection():
+    """Returns a database connection"""
+    return MySQLdb.Connect(
+        db=current_app.config.get("DB_NAME"),
+        user=current_app.config.get("DB_USER"),
+        passwd=current_app.config.get("DB_PASS"),
+        host=current_app.config.get("DB_HOST"))
--- wqflask/wqflask/partial_correlations_views.py
+++ wqflask/wqflask/partial_correlations_views.py
@@ -1,142 +1,31 @@
+import json
+import math
+import requests
+from functools import reduce
 from typing import Union, Tuple
 
-import MySQLdb
-from gn3.db.traits import retrieve_trait_info
-from flask import flash, request, current_app, render_template
-from gn3.computations.partial_correlations import partial_correlations_entry
+from flask import (
+    flash,
+    request,
+    url_for,
+    redirect,
+    current_app,
+    render_template)
 
 from wqflask import app
 from utility.tools import GN_SERVER_URL
+from wqflask.database import database_connection
+from gn3.db.partial_correlations import traits_info
 
-def parse_trait(trait_str: str) -> Union[dict, None]:
-    keys = (
-        "name", "dataset", "symbol", "description", "location", "mean_expr",
-        "max_lrs", "data_hmac")
-    parts = tuple(part.strip() for part in trait_str.split(":::"))
-    if len(parts) == len(keys):
-        return dict(zip(keys, parts))
-    return None
-
-def process_step_select_primary(
-        primary_trait: dict, control_traits: Tuple[dict, ...],
-        target_traits: Tuple[dict, ...],
-        traits_list: Tuple[dict, ...], corr_method: str) -> Tuple[
-            str, dict, Tuple[dict, ...], Tuple[dict, ...], Tuple[dict, ...],
-            str]:
-    if primary_trait is None:
-        flash("You must select a primary trait", "alert-danger")
-        return (
-            "select-primary", primary_trait, control_traits, target_traits,
-            traits_list, corr_method)
-
-    return (
-        "select-controls", primary_trait, control_traits, target_traits,
-        tuple(
-            trait for trait in traits_list
-            if trait["data_hmac"] != primary_trait["data_hmac"]),
-        corr_method)
-
-def process_step_select_controls(
-        primary_trait: dict, control_traits: Tuple[dict, ...],
-        target_traits: Tuple[dict, ...],
-        traits_list: Tuple[dict, ...], corr_method: str) -> Tuple[
-            str, dict, Tuple[dict, ...], Tuple[dict, ...], Tuple[dict, ...],
-            str]:
-    if len(control_traits) == 0 or len(control_traits) > 3:
-        flash(
-            ("You must select a minimum of one control trait, up to a maximum "
-             "of three control traits."),
-            "alert-danger")
-        return (
-            "select-controls", primary_trait, control_traits, target_traits,
-            traits_list, corr_method)
-
-    hmacs =(primary_trait["data_hmac"],) + tuple(
-        trait["data_hmac"] for trait in control_traits)
-    return (
-        "select-corr-method", primary_trait, control_traits, target_traits,
-        tuple(
-            trait for trait in traits_list if trait["data_hmac"] not in hmacs),
-        corr_method)
-
-def process_step_select_targets(
-        primary_trait: dict, control_traits: Tuple[dict, ...],
-        target_traits: Tuple[dict, ...],
-        traits_list: Tuple[dict, ...], corr_method: str) -> Tuple[
-            str, dict, Tuple[dict, ...], Tuple[dict, ...], Tuple[dict, ...],
-            str]:
-    if len(target_traits) == 0:
-        flash(
-            "You must select at least one target trait.", "alert-danger")
-        return (
-            "select-targets", primary_trait, control_traits, target_traits,
-            traits_list, corr_method)
-
-    hmacs =(primary_trait["data_hmac"],) + tuple(
-        trait["data_hmac"] for trait in (control_traits + target_traits))
-    return (
-        "select-corr-method", primary_trait, control_traits, target_traits,
-        tuple(
-            trait for trait in traits_list if trait["data_hmac"] not in hmacs),
-        corr_method)
-
-def process_step_select_corr_method(
-        primary_trait: dict, control_traits: Tuple[dict, ...],
-        target_traits: Tuple[dict, ...],
-        traits_list: Tuple[dict, ...], corr_method: str) -> Tuple[
-            str, dict, Tuple[dict, ...], Tuple[dict, ...], Tuple[dict, ...],
-            str]:
-    methods = (
-        "genetic correlation, pearson's r",
-        "genetic correlation, spearman's rho",
-        "sgo literature correlation",
-        "tissue correlation, pearson's r",
-        "tissue correlation, spearman's rho")
-    if corr_method.lower() not in methods:
-        flash(
-            "Selected method is unknown.", "alert-danger")
-        return (
-            "select-corr-method", primary_trait, control_traits, target_traits,
-            traits_list, corr_method)
-
-    hmacs =(primary_trait["data_hmac"],) + tuple(
-        trait["data_hmac"] for trait in (control_traits + target_traits))
-    return (
-        "run-correlation", primary_trait, control_traits, target_traits,
-        tuple(
-            trait for trait in traits_list if trait["data_hmac"] not in hmacs),
-        corr_method)
-
-def process_step(
-        step: str, primary_trait: dict, control_traits: Tuple[dict, ...],
-        target_traits: Tuple[dict, ...], traits_list: Tuple[dict, ...],
-        corr_method: str) -> Tuple[
-            str, dict, Tuple[dict, ...], Tuple[dict, ...], Tuple[dict, ...],
-            str]:
-    processor_functions = {
-        # "select-traits": lambda arg: arg,
-        "select-primary": process_step_select_primary,
-        "select-controls": process_step_select_controls,
-        "select-targets": process_step_select_targets,
-        "select-corr-method": process_step_select_corr_method
-    }
-    return processor_functions[(step or "select-primary")](
-        primary_trait, control_traits, target_traits, traits_list, corr_method)
-
-def sequence_of_traits(trait_strs) -> Tuple[dict, ...]:
-    return tuple(filter(
-        lambda trt: trt is not None,
-        (parse_trait(tstr.strip()) for tstr in trait_strs)))
-
-def publish_target_dabases(conn, group, threshold):
+def publish_target_databases(conn, groups, threshold):
     query = (
         "SELECT PublishFreeze.FullName,PublishFreeze.Name "
         "FROM PublishFreeze, InbredSet "
         "WHERE PublishFreeze.InbredSetId = InbredSet.Id "
-        "AND InbredSet.Name = %(group)s "
-        "AND PublishFreeze.public > %(threshold)s")
+        f"AND InbredSet.Name IN ({', '.join(['%s'] * len(groups))}) "
+        "AND PublishFreeze.public > %s")
     with conn.cursor() as cursor:
-        cursor.execute(query, {"group": group, "threshold": threshold})
+        cursor.execute(query, tuple(groups) + (threshold,))
         res = cursor.fetchall()
         if res:
             return tuple(
@@ -144,15 +33,15 @@ def publish_target_dabases(conn, group, threshold):
 
     return tuple()
 
-def geno_target_databases(conn, group, threshold):
+def geno_target_databases(conn, groups, threshold):
     query = (
         "SELECT GenoFreeze.FullName,GenoFreeze.Name "
         "FROM GenoFreeze, InbredSet "
         "WHERE GenoFreeze.InbredSetId = InbredSet.Id "
-        "AND InbredSet.Name = %(group)s "
-        "AND GenoFreeze.public > %(threshold)s")
+        f"AND InbredSet.Name IN ({', '.join(['%s'] * len(groups))}) "
+        "AND GenoFreeze.public > %s")
     with conn.cursor() as cursor:
-        cursor.execute(query, {"group": group, "threshold": threshold})
+        cursor.execute(query, tuple(groups) + (threshold,))
         res = cursor.fetchall()
         if res:
             return tuple(
@@ -160,27 +49,26 @@ def geno_target_databases(conn, group, threshold):
 
     return tuple()
 
-def probeset_target_databases(conn, group, threshold):
+def probeset_target_databases(conn, groups, threshold):
     query1 = "SELECT Id, Name FROM Tissue order by Name"
-    query2 = (
-        "SELECT ProbeFreeze.TissueId, ProbeSetFreeze.FullName, ProbeSetFreeze.Name "
-        "FROM ProbeSetFreeze, ProbeFreeze, InbredSet "
-        "WHERE ProbeSetFreeze.ProbeFreezeId = ProbeFreeze.Id "
-        "AND ProbeFreeze.TissueId IN %(tissue_ids)s "
-        "AND ProbeSetFreeze.public > %(threshold)s "
-        "AND ProbeFreeze.InbredSetId = InbredSet.Id "
-        "AND InbredSet.Name like %(group)s "
-        "ORDER BY ProbeSetFreeze.CreateTime desc, ProbeSetFreeze.AvgId")
     with conn.cursor() as cursor:
         cursor.execute(query1)
         tissue_res = cursor.fetchall()
         if tissue_res:
             tissue_ids = tuple(row[0] for row in tissue_res)
-            cursor.execute(
-                query2,{
-                    "tissue_ids": tissue_ids, "group": f"{group}%%",
-                    "threshold": threshold
-                })
+            groups_clauses = ["InbredSet.Name like %s"] * len(groups)
+            query2 = (
+                "SELECT ProbeFreeze.TissueId, ProbeSetFreeze.FullName, "
+                "ProbeSetFreeze.Name "
+                "FROM ProbeSetFreeze, ProbeFreeze, InbredSet "
+                "WHERE ProbeSetFreeze.ProbeFreezeId = ProbeFreeze.Id "
+                "AND ProbeFreeze.TissueId IN "
+                f"({', '.join(['%s'] * len(tissue_ids))}) "
+                "AND ProbeSetFreeze.public > %s "
+                "AND ProbeFreeze.InbredSetId = InbredSet.Id "
+                f"AND ({' OR '.join(groups_clauses)}) "
+                "ORDER BY ProbeSetFreeze.CreateTime desc, ProbeSetFreeze.AvgId")
+            cursor.execute(query2, tissue_ids + (threshold,) + tuple(groups))
             db_res = cursor.fetchall()
             if db_res:
                 databases = tuple(
@@ -197,70 +85,201 @@ def probeset_target_databases(conn, group, threshold):
 
     return tuple()
 
-def target_databases(conn, step, trait, threshold):
+def target_databases(conn, traits, threshold):
     """
     Retrieves the names of possible target databases from the database.
     """
-    if step != "select-corr-method":
-        return None
-
-    trait_info = retrieve_trait_info(
-        threshold, f"{trait['dataset']}::{trait['name']}", conn)
-    group = trait_info["group"]
+    trait_info = traits_info(
+        conn, threshold,
+        tuple(f"{trait['dataset']}::{trait['trait_name']}" for trait in traits))
+    groups = tuple(set(row["db"]["group"] for row in trait_info))
     return (
-        publish_target_dabases(conn, group, threshold) +
-        geno_target_databases(conn, group, threshold) +
-        probeset_target_databases(conn, group, threshold))
+        publish_target_databases(conn, groups, threshold) +
+        geno_target_databases(conn, groups, threshold) +
+        probeset_target_databases(conn, groups, threshold))
 
-def pcorrelations(conn, values):
-    if values["step"] != "run-correlation":
-        return None
+def primary_error(args):
+    if len(args["primary_trait"]) == 0 or len(args["primary_trait"]) > 1:
+        return {
+            **args,
+            "errors": (args.get("errors", tuple()) +
+                       ("You must provide one, and only one primary trait",))}
+    return args
 
-    def trait_fullname(trait):
-        return f"{trait['dataset']}::{trait['name']}"
+def controls_error(args):
+    if len(args["control_traits"]) == 0 or len(args["control_traits"]) > 3:
+        return {
+            **args,
+            "errors": (
+                args.get("errors", tuple()) +
+                (("You must provide at least one control trait, and a maximum "
+                  "of three control traits"),))}
+    return args
 
-    return partial_correlations_entry(
-        conn, trait_fullname(values["primary_trait"]),
-        tuple(trait_fullname(trait) for trait in values["control_traits"]),
-        values["method"], values["criteria"], values["target_db"])
+def target_db_error(args):
+    if not args["target_db"]:
+        return {
+            **args,
+            "errors": (
+                args.get("errors", tuple()) +
+                ("The target database must be provided",))}
+    return args
+
+def method_error(args):
+    methods = (
+        "genetic correlation, pearson's r",
+        "genetic correlation, spearman's rho",
+        "sgo literature correlation",
+        "tissue correlation, pearson's r",
+        "tissue correlation, spearman's rho")
+    if not args["method"] or args["method"].lower() not in methods:
+        return {
+            **args,
+            "errors": (
+                args.get("errors", tuple()) +
+                ("Invalid correlation method provided",))}
+    return args
+
+def criteria_error(args):
+    try:
+        int(args.get("criteria", "invalid"))
+        return args
+    except ValueError:
+        return {
+            **args,
+            "errors": (
+                args.get("errors", tuple()) +
+                ("Invalid return number provided",))}
+
+def errors(args):
+    return criteria_error(method_error(target_db_error(controls_error(
+        primary_error(args)))))
+
+def __classify_args(acc, item):
+    if item[1].startswith("primary_"):
+        return {
+            **acc,
+            "primary_trait": (acc.get("primary_trait", tuple()) + (item,))} 
+    if item[1].startswith("controls_"):
+        return {**acc, "control_traits": (acc.get("control_traits", tuple()) + (item,))}
+    if item[0] == "target_db":
+        return {**acc, "target_db": item[1]}
+    if item[0] == "method":
+        return {**acc, "method": item[1]}
+    if item[0] == "criteria":
+        return {**acc, "criteria": item[1]}
+    return acc
+
+def __build_args(raw_form, traits):
+    args = reduce(__classify_args, raw_form.items(), {})
+    return {
+        **args,
+        "primary_trait": [
+            item for item in traits if item["trait_name"] in
+            (name[1][8:] for name in args["primary_trait"])],
+        "control_traits": [
+            item for item in traits if item["trait_name"] in
+            (name[1][9:] for name in args["control_traits"])]
+    }
+
+def parse_trait(trait_str):
+    return dict(zip(
+        ("trait_name", "dataset", "description", "symbol", "location", "mean",
+         "lrs", "lrs_location"),
+        trait_str.strip().split("|||")))
+
+def response_error_message(response):
+    error_messages = {
+        404: ("We could not connect to the API server at this time. "
+              "Try again later."),
+        500: ("The API server experienced a problem. We will be working on a "
+              "fix. Please try again later.")
+    }
+    return error_messages.get(
+        response.status_code,
+        "General API server error!!")
+
+def render_error(error_message):
+    return render_template(
+        "partial_correlations/pcorrs_error.html",
+        message = error_message)
+
+def handle_200_response(response):
+    if response["status"] == "success":
+        return redirect(
+            url_for(
+                "poll_partial_correlation_results",
+                command_id=response["results"]),
+            code=303)
+    return render_error(response["results"])
+
+def handle_response(response):
+    if response.status_code != 200:
+        return render_template(
+            "partial_correlations/pcorrs_error.html",
+            message = response_error_message(response))
+    return handle_200_response(response.json())
 
 @app.route("/partial_correlations", methods=["POST"])
 def partial_correlations():
     form = request.form
-    traits_list = tuple(filter(
-        lambda trt: trt is not None,
-        (parse_trait(tstr) for tstr in form.get("traits_list", "").split("|||"))))
+    traits = tuple(
+        parse_trait(trait) for trait in
+        form.get("trait_list").split(";;;"))
 
-    args_dict = dict(zip(
-        ("step", "primary_trait", "control_traits", "target_traits",
-         "traits_list", "method"),
-        process_step(
-            form.get("step", None),
-            parse_trait(form.get("primary_trait", "")),
-            sequence_of_traits(
-                form.getlist("control_traits[]") or
-                form.get("control_traits", "").split("|||")),
-            sequence_of_traits(
-                form.getlist("target_traits[]") or
-                form.get("target_traits", "").split("|||")),
-            sequence_of_traits(form.get("traits_list", "").split("|||")),
-            form.get("method"))))
+    if form.get("submit") == "Run Partial Correlations":
+        args = errors(__build_args(form, traits))
+        if len(args.get("errors", [])) == 0:
+            post_data = {
+                **args,
+                "primary_trait": args["primary_trait"][0]
+            }
+            return handle_response(requests.post(
+                url=f"{GN_SERVER_URL}api/correlation/partial",
+                json=json.dumps(post_data)))
 
-    conn = MySQLdb.Connect(
-        db=current_app.config.get("DB_NAME"),
-        user=current_app.config.get("DB_USER"),
-        passwd=current_app.config.get("DB_PASS"),
-        host=current_app.config.get("DB_HOST"))
-    target_dbs = target_databases(
-        conn, args_dict["step"], args_dict["primary_trait"], 0)
+        for error in args["errors"]:
+            flash(error, "alert-danger")
 
-    if args_dict["step"] == "run-correlation":
-        args_dict = {
-            **args_dict, "target_db": form.get("target_db"),
-            "criteria": int(form.get("criteria", 500))}
+    with database_connection() as conn:
+        target_dbs = target_databases(conn, traits, threshold=0)
+        return render_template(
+            "partial_correlations/pcorrs_select_operations.html",
+            trait_list_str=form.get("trait_list"),
+            traits=traits,
+            target_dbs=target_dbs)
 
-    corr_results = pcorrelations(conn, args_dict)
+def process_pcorrs_command_output(result):
+    if result["status"] == "success":
+        def __format_number(num):
+            if num is None or math.isnan(num):
+                return ""
+            if abs(num) <= 1.04E-4:
+                return f"{num:.2e}"
+            return f"{num:.5f}"
 
-    return render_template(
-        "partial_correlations.html", **args_dict, target_dbs=target_dbs,
-        corr_results=corr_results, part_corr_url=f"{GN_SERVER_URL}api/correlation/partial")
+        return render_template(
+            "partial_correlations/pcorrs_results_presentation.html",
+            primary=result["results"]["primary_trait"],
+            controls=result["results"]["control_traits"],
+            correlations=result["results"]["correlations"],
+            dataset_type=result["results"]["dataset_type"],
+            method=result["results"]["method"],
+            format_number=__format_number)
+    if result["status"] == "error":
+        return render_error(
+            "The partial correlations computation failed with an error")
+
+@app.route("/partial_correlations/<command_id>", methods=["GET"])
+def poll_partial_correlation_results(command_id):
+    response = requests.get(
+        url=f"{GN_SERVER_URL}api/async_commands/state/{command_id}")
+    if response.status_code == 200:
+        data = response.json()
+        if data["status"] == "error":
+            return render_error(response["result"])
+        if data["status"] == "success":
+            return process_pcorrs_command_output(json.loads(data["result"]))
+        return render_template(
+            "partial_correlations/pcorrs_poll_results.html",
+            command_id = command_id)
