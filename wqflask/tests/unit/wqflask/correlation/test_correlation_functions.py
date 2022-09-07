f879f82ad0393a770ed50043c70ee1dd4a12daaa
Munyoki Kilyungi <me@bonfacemunyoki.com>
2022-09-07 11:18:06 +0300

    Rewrite "test_tissue_corr_computation" as a pytest
    * wqflask/tests/unit/wqflask/correlation/test_correlation_functions.py
    (TestCorrelationFunctions): Convert this unittest class to ...
    (test_tissue_corr_computation): ... a pytest function.

--- wqflask/tests/unit/wqflask/correlation/test_correlation_functions.py
+++ wqflask/tests/unit/wqflask/correlation/test_correlation_functions.py
@@ -7,24 +7,19 @@ from wqflask.correlation.correlation_functions import get_trait_symbol_and_tissu
 from wqflask.correlation.correlation_functions import cal_zero_order_corr_for_tiss
 
 
-class TestCorrelationFunctions(unittest.TestCase):
-    """test for correlation helper functions"""
+def test_tissue_corr_computation(mocker):
+    """Test for cal_zero_order_corr_for_tiss"""
+    primary_values = [9.288, 9.313, 8.988, 9.660, 8.21]
+    target_values = [9.586, 8.498, 9.362, 8.820, 8.786]
+    _m = mocker.patch(("wqflask.correlation.correlation_functions."
+                       "compute_corr_coeff_p_value"),
+                      return_value=(0.51, 0.7))
+    results = cal_zero_order_corr_for_tiss(primary_values, target_values)
+    _m.assert_called_once_with(
+        primary_values=primary_values, target_values=target_values,
+        corr_method="pearson")
+    assert len(results) == 3
 
-    @mock.patch("wqflask.correlation.correlation_functions.compute_corr_coeff_p_value")
-    def test_tissue_corr_computation(self, mock_tiss_corr_computation):
-        """test for cal_zero_order_corr_for_tiss"""
-
-        primary_values = [9.288, 9.313, 8.988, 9.660, 8.21]
-        target_values = [9.586, 8.498, 9.362, 8.820, 8.786]
-
-        mock_tiss_corr_computation.return_value = (0.51, 0.7)
-
-        results = cal_zero_order_corr_for_tiss(primary_values, target_values)
-        mock_tiss_corr_computation.assert_called_once_with(
-            primary_values=primary_values, target_values=target_values,
-            corr_method="pearson")
-
-        self.assertEqual(len(results), 3)
 
     @mock.patch("wqflask.correlation.correlation_functions.MrnaAssayTissueData")
     def test_get_trait_symbol_and_tissue_values(self, mock_class):
