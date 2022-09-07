8149d9ceed6eb48cbfa192a675fa8a10139a5b8e
Munyoki Kilyungi <me@bonfacemunyoki.com>
2022-09-07 11:51:38 +0300

    Move test case to mrna_assay_tissue_data test module
    * wqflask/tests/unit/base/test_mrna_assay_tissue_data.py
    * wqflask/tests/unit/wqflask/correlation/test_correlation_functions.py
    (TestCorrelationFunctions.test_tissue_corr_computation): Delete.
    (TestCorrelationFunctions): Move this ...
    (test_mrna_assay_tissue_data_initialisation): ... here

--- wqflask/tests/unit/wqflask/correlation/test_correlation_functions.py
+++ wqflask/tests/unit/wqflask/correlation/test_correlation_functions.py
@@ -19,17 +19,3 @@ def test_tissue_corr_computation(mocker):
         primary_values=primary_values, target_values=target_values,
         corr_method="pearson")
     assert len(results) == 3
-
-
-    @mock.patch("wqflask.correlation.correlation_functions.MrnaAssayTissueData")
-    def test_get_trait_symbol_and_tissue_values(self, mock_class):
-        """test for getting trait symbol and tissue_values"""
-        mock_class_instance = mock_class.return_value
-        mock_class_instance.gene_symbols = ["k1", "k2", "k3"]
-        mock_class_instance.get_symbol_values_pairs.return_value = {
-            "k1": ["v1", "v2", "v3"], "k2": ["v2", "v3"], "k3": ["k3"]}
-        results = get_trait_symbol_and_tissue_values(
-            symbol_list=["k1", "k2", "k3"])
-        mock_class.assert_called_with(gene_symbols=['k1', 'k2', 'k3'])
-        self.assertEqual({"k1": ["v1", "v2", "v3"], "k2": [
-                         "v2", "v3"], "k3": ["k3"]}, results)
