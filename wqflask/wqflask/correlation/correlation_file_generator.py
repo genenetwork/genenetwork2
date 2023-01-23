1e4eafe104fd55d4ffe7b2391d542260db714f31
Alexander_Kabui <alexanderkabua@gmail.com>
2023-01-23 13:10:46 +0300

    fetch all  strain names

--- wqflask/wqflask/correlation/correlation_file_generator.py
+++ wqflask/wqflask/correlation/correlation_file_generator.py
@@ -1,13 +1,26 @@
-def get_probesetfreezes(conn,inbredsetid=1):
+def get_probesetfreezes(conn, inbredsetid=1):
+    with conn.cursor() as cursor:
+        cursor.execute(
+            "SELECT ProbeSetFreeze.Id, ProbeSetFreeze.Name, ProbeSetFreeze.FullName "
+            "FROM ProbeSetFreeze, ProbeFreeze "
+            "WHERE ProbeSetFreeze.ProbeFreezeId=ProbeFreeze.Id "
+            "AND ProbeFreeze.InbredSetId=%s",
+            (inbredsetid,)
+        )
+
+        return cursor.fetchall()
 
 
-	with conn.cursor() as cursor:
-		cursor.execute(
-			"SELECT ProbeSetFreeze.Id, ProbeSetFreeze.Name, ProbeSetFreeze.FullName "
-    		"FROM ProbeSetFreeze, ProbeFreeze "
-    		"WHERE ProbeSetFreeze.ProbeFreezeId=ProbeFreeze.Id "
-    		"AND ProbeFreeze.InbredSetId=%s",
-    		(inbredsetid,)
-			)
+def get_strains(conn, inbredsetid=1):
 
-		return  cursor.fetchall() 
+    with conn.cursor() as cursor:
+        cursor.execute(
+            "SELECT Strain.Id, Strain.Name "
+            "FROM StrainXRef, Strain "
+            "WHERE StrainXRef.InbredSetId=%s "
+            "AND StrainXRef.StrainId=Strain.Id "
+            "ORDER BY StrainXRef.OrderId",
+            (inbredsetid)
+        )
+
+        return cursor.fetchall()
