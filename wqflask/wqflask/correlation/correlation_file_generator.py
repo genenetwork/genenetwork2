b83e41162918619c11c475602743a682b6abd4c3
Alexander_Kabui <alexanderkabua@gmail.com>
2023-01-23 13:13:11 +0300

    fetch id for a given probeset

--- wqflask/wqflask/correlation/correlation_file_generator.py
+++ wqflask/wqflask/correlation/correlation_file_generator.py
@@ -25,18 +25,31 @@ def get_strains(conn, inbredsetid=1):
 
         return cursor.fetchall()
 
+
 def fetch_datasets(conn):
 
-	#fi parents included?????
-	with conn.cursor() as cursor:
-	    cursor.execute(
-	        "SELECT ProbeSet.Name, Strain.Name, ProbeSetData.value "
-	        "FROM Strain LEFT JOIN ProbeSetData "
-	        "ON Strain.Id = ProbeSetData.StrainId "
-	        "LEFT JOIN ProbeSetXRef ON ProbeSetData.Id = ProbeSetXRef.DataId "
-	        "LEFT JOIN ProbeSet ON ProbeSetXRef.ProbeSetId = ProbeSet.Id "
-	        "WHERE ProbeSetXRef.ProbeSetFreezeId IN "
-	        "(SELECT Id FROM ProbeSetFreeze WHERE Name = %s) "
-	        "ORDER BY Strain.Name",
-	        (db_name,))
-	    return cursor.fetchall()
+    # fi parents included?????
+    with conn.cursor() as cursor:
+        cursor.execute(
+            "SELECT ProbeSet.Name, Strain.Name, ProbeSetData.value "
+            "FROM Strain LEFT JOIN ProbeSetData "
+            "ON Strain.Id = ProbeSetData.StrainId "
+            "LEFT JOIN ProbeSetXRef ON ProbeSetData.Id = ProbeSetXRef.DataId "
+            "LEFT JOIN ProbeSet ON ProbeSetXRef.ProbeSetId = ProbeSet.Id "
+            "WHERE ProbeSetXRef.ProbeSetFreezeId IN "
+            "(SELECT Id FROM ProbeSetFreeze WHERE Name = %s) "
+            "ORDER BY Strain.Name",
+            (db_name,))
+        return cursor.fetchall()
+
+
+def get_probesetfreeze(conn, probes):
+
+    with conn.cursor() as cursor:
+        cursor.execute(
+            "SELECT ProbeSetFreeze.Id, ProbeSetFreeze.Name, ProbeSetFreeze.FullName "
+            "FROM ProbeSetFreeze "
+            "WHERE ProbeSetFreeze.Id=%s",
+            (probes,)
+        )
+        return cursor.fetchone()
