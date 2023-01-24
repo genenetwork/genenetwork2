f9fcbc26c5588f62f5c674d9d424197b9c8b1fc4
Frederick Muriuki Muriithi <fredmanglis@gmail.com>
2023-01-25 02:46:53 +0300

    mechanical-rob: corrs: Activate tests. Print out whitespace
    * Activate the correlation correctness tests
    * Print out whitespace characters in such a form that allows visual
      comparison of differences to help with fixing issues caught by
      tests.

--- test/requests/correlation_tests.py
+++ test/requests/correlation_tests.py
@@ -172,9 +172,9 @@ def collect_failures(actual, expected, keys):
                 continue
             __eq = __eq + ((
                 f"Trait '{trait_id}': "
-                f"Different '{title}' values: expected:\n\t\t'{exp_val}'"
+                f"Different '{title}' values: expected:\n\t\t'{repr(exp_val)}'"
                 "\n\nbut got\n"
-                f"\n\t\t'{act_val}'"),)
+                f"\n\t\t'{repr(act_val)}'"),)
             continue
         return __eq
 
