5df9f0400f5457071a6e717ba68d72454f6f07f5
Frederick Muriuki Muriithi <fredmanglis@gmail.com>
2023-05-10 13:19:01 +0300

    auth: bug-fix: Call `logged_in` function
    The `logged_in` symbol was changed from a plain variable into a
    function, so we need to call it to get its actual value.

--- wqflask/wqflask/oauth2/ui.py
+++ wqflask/wqflask/oauth2/ui.py
@@ -8,7 +8,7 @@ def render_ui(templatepath: str, **kwargs):
     """Handle repetitive UI rendering stuff."""
     logged_in = lambda: ("oauth2_token" in session and bool(session["oauth2_token"]))
     roles = kwargs.get("roles", tuple()) # Get roles if already provided
-    if logged_in and not bool(roles): # If not, try fetching them
+    if logged_in() and not bool(roles): # If not, try fetching them
         roles_results = oauth2_get("oauth2/user/roles").either(
             lambda err: {"roles_error": process_error(err)},
             lambda roles: {"roles": roles})
