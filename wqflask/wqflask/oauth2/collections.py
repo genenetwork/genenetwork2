dd0673f95ed3508d42c93fd9cdfc2523979db284
Frederick Muriuki Muriithi <fredmanglis@gmail.com>
2023-12-18 16:16:01 +0300

    Fix typo.

--- wqflask/wqflask/oauth2/collections.py
+++ wqflask/wqflask/oauth2/collections.py
@@ -4,7 +4,7 @@ from .checks import user_logged_in
 from .client import oauth2_get, no_token_get
 
 def num_collections() -> int:
-    """Compute the number of collections available for tte current sussion."""
+    """Compute the number of collections available for the current session."""
     anon_id = session_info()["anon_id"]
     all_collections = no_token_get(
         f"auth/user/collections/{anon_id}/list").either(
