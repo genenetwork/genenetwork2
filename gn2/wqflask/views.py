c7b1210c1a8716ef045a1291bca76c1d822c134f
Alexander_Kabui <alexanderkabua@gmail.com>
2024-05-28 17:51:59 +0300

    BugFix: return correct status code for generic exceptions handler.

--- gn2/wqflask/views.py
+++ gn2/wqflask/views.py
@@ -137,6 +137,7 @@ def handle_generic_exceptions(e):
                                          stack={formatted_lines},
                                          error_image=animation,
                                          version=current_app.config.get("GN_VERSION")))
+    resp.status_code = exc_type.code or 500
     resp.set_cookie(err_msg[:32], animation)
     return resp
 
