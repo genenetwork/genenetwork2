bb83fdde2cff7abd8571350966fa78545b1831ef
Frederick Muriuki Muriithi <fredmanglis@gmail.com>
2024-06-17 13:56:29 -0500

    Delete request to obsoleted endpoint.

--- gn2/wqflask/oauth2/ui.py
+++ gn2/wqflask/oauth2/ui.py
@@ -7,12 +7,7 @@ from .request_utils import process_error
 
 def render_ui(templatepath: str, **kwargs):
     """Handle repetitive UI rendering stuff."""
-    roles = kwargs.get("roles", tuple()) # Get roles if already provided
-    if user_logged_in() and not bool(roles): # If not, try fetching them
-        roles_results = oauth2_get("auth/system/roles").either(
-            lambda err: {"roles_error": process_error(err)},
-            lambda roles: {"roles": roles})
-        kwargs = {**kwargs, **roles_results}
+    roles = kwargs.get("roles", tuple()) # Get roles
     user_privileges = tuple(
         privilege["privilege_id"] for role in roles
         for privilege in role["privileges"])
