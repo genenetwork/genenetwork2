6d763b5c8aece1b0442fecbc659c4da3456b4e75
John Nduli <rookie101@jnduli.co.ke>
2024-08-07 09:32:16 +0300

    chore: remove passing bearer token to end user

--- gn2/wqflask/oauth2/ui.py
+++ gn2/wqflask/oauth2/ui.py
@@ -1,11 +1,7 @@
 """UI utilities"""
-from flask import session, render_template
-
-from gn2.wqflask.oauth2 import session
+from flask import render_template
 
 from .client import oauth2_get
-from .client import user_logged_in
-from .request_utils import process_error
 
 def render_ui(templatepath: str, **kwargs):
     """Handle repetitive UI rendering stuff."""
@@ -15,8 +11,7 @@ def render_ui(templatepath: str, **kwargs):
                 lambda _err: roles, lambda auth_roles: auth_roles)
     user_privileges = tuple(
         privilege["privilege_id"] for role in roles for privilege in role["privileges"])
-    user_token = session.user_token().either(lambda _err: "", lambda token: token["access_token"])
     kwargs = {
-            **kwargs, "roles": roles, "user_privileges": user_privileges, "bearer_token": user_token
+            **kwargs, "roles": roles, "user_privileges": user_privileges,
     }
     return render_template(templatepath, **kwargs)
