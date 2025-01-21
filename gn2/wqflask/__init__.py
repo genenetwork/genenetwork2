b12a22fc116e7ea298899e32291fbcf09be4a04e
Munyoki Kilyungi <me@bonfacemunyoki.com>
2025-01-21 08:54:55 +0300

    Disable strict trailing slashes.
    Signed-off-by: Munyoki Kilyungi <me@bonfacemunyoki.com>

--- gn2/wqflask/__init__.py
+++ gn2/wqflask/__init__.py
@@ -111,6 +111,8 @@ app.jinja_env.globals.update(
     num_collections=numcoll,
     datetime=datetime)
 
+app.url_map.strict_slashes = False
+
 
 # Registering blueprints
 app.register_blueprint(gn_docs_blueprint, url_prefix="/gn-docs")
