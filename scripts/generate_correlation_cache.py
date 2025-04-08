f79f689f9187b07470969f64606d6c9fc353a0b3
Munyoki Kilyungi <me@bonfacemunyoki.com>
2025-04-08 12:14:25 +0300

    Write checksum of tables used in Probeset cache to a file.
    Signed-off-by: Munyoki Kilyungi <me@bonfacemunyoki.com>

--- scripts/generate_correlation_cache.py
+++ scripts/generate_correlation_cache.py
@@ -7,6 +7,7 @@ import time
 import datetime
 import logging
 import click
+import hashlib
 
 from gn2.wqflask.correlation.pre_computes import write_db_to_textfile
 from gn2.base.webqtlConfig import CACHEDIR
@@ -15,6 +16,16 @@ from gn2.utility.tools import SQL_URI, assert_writable_dir
 
 
 assert_writable_dir(CACHEDIR)
+
+
+def get_cache_checksum():
+    """Get a checksum for the Tables: Strain, ProbeSetFreeze,
+    ProbeSet, ProbeSetData, ProbeSetXRef as a single md5 hash"""
+    with database_connection(SQL_URI) as conn, conn.cursor() as cursor:
+        cursor.execute("CHECKSUM Table Strain, ProbeSetFreeze, \
+        ProbeSet, ProbeSetData, ProbeSetXRef")
+        checksums = [str(checksum) for (_, checksum) in cursor.fetchall()]
+        return hashlib.md5(" ".join(checksums).encode("utf-8")).hexdigest()
 
 
 @click.command(help="Read all the Probeset data and cache it into CACHEDIR")
@@ -37,6 +48,10 @@ def build_probeset_cache():
             except Exception as e:
                 logging.error(f"Error processing {name}: {e}", exc_info=True)
     index_time = datetime.timedelta(seconds=time.perf_counter() - start_time)
+
+    with open(os.path.join(CACHEDIR, "CHECKSUM.txt"), "w") as checksum:
+        checksum.write(get_cache_checksum())
+
     logging.info("Cache files successfully built.")
     logging.info(f"Time to build cache: {index_time}")
 
