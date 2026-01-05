be961fecb489af843db5e95e09daed85fa2b3a7e
Frederick Muriuki Muriithi <fredmanglis@gmail.com>
2026-01-05 12:04:19 -0600

    mechanical-rob: Partial corrs, Spearmans's Rho, whole dataset
    Implement partial correlations against all traits in a particular
    dataset using the Spearman's Rho method.

--- test/requests/partial_correlations_tests.py
+++ test/requests/partial_correlations_tests.py
@@ -163,9 +163,25 @@ def check_pc_against_entire_dataset_pearsons(baseurl):
 
 def check_pc_against_entire_dataset_spearmans(baseurl):
     """Check partial correlations against entire dataset using Spearman's rho."""
-    print(f"\tERROR — Non fatal (Spearmans - whole dataset): Please implement this test:", end="\t")
-    # TODO: Change prompt above
-    # TODO: Implement test below, exit with sys.exit(1) on error
+    print(f"\t(Spearman's Rho - whole dataset):", end="\t")
+    traits = start_traits(True)
+    result = do_request(urljoin(baseurl, "/partial_correlations"),
+                        postdata={
+                            "trait_list": start_traits(False),
+                            **__select_primary_controls_targets__(
+                                traits, 0, (1, 2), (3, 4)),
+                            "method": "Spearman's Rho",
+                            "criteria": 500,
+                            "submit": "Run Partial Correlations",
+                            "target_db": "BXDPublish"
+                        })
+    assert result.status_code == 200, (
+        f"Status code was not 200, it was {result.status_code}")
+
+    doc = etree.HTML(result.text)
+    table_rows = doc.xpath("//table[@id='part-corr-results-publish']/tbody/tr")
+    assert len(table_rows) >= 5, (
+        f"Expected at least 5 rows of results. Got {len(row)}")
     print("OK")
 
 
