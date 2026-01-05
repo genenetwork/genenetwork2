1c0d85dd3a43c5bdf93e26eb2f699aaf307ffe65
Frederick Muriuki Muriithi <fredmanglis@gmail.com>
2026-01-05 11:33:05 -0600

    Distinguish non-implemented tests.

--- test/requests/partial_correlations_tests.py
+++ test/requests/partial_correlations_tests.py
@@ -139,7 +139,7 @@ def check_pc_against_specific_traits_spearmans(baseurl):
 
 def check_pc_against_entire_dataset_pearsons(baseurl):
     """Check partial correlations against entire dataset using Pearson's r."""
-    print(f"\tERROR — Non fatal: Please implement this test:", end="\t")
+    print(f"\tERROR — Non fatal (Pearsons - whole dataset): Please implement this test:", end="\t")
     # TODO: Change prompt above
     # TODO: Implement test below, exit with sys.exit(1) on error
     print("OK")
@@ -147,7 +147,7 @@ def check_pc_against_entire_dataset_pearsons(baseurl):
 
 def check_pc_against_entire_dataset_spearmans(baseurl):
     """Check partial correlations against entire dataset using Spearman's rho."""
-    print(f"\tERROR — Non fatal: Please implement this test:", end="\t")
+    print(f"\tERROR — Non fatal (Spearmans - whole dataset): Please implement this test:", end="\t")
     # TODO: Change prompt above
     # TODO: Implement test below, exit with sys.exit(1) on error
     print("OK")
