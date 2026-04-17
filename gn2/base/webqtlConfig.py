708e02c9eaefd6d512df97c11b7f8a1f36be9d54
zsloan <zachary.a.sloan@gmail.com>
2026-04-17 11:05:05 -0500

    Add 'BXD_UT-SJ' to BXD_GROUP_EXCEPTIONS

--- gn2/base/webqtlConfig.py
+++ gn2/base/webqtlConfig.py
@@ -34,7 +34,7 @@ MAXLRS = 460.0
 PUBLICTHRESH = 0
 
 # Groups to treat as unique when drawing correlation dropdowns (not sure if this logic even makes sense or is necessary)
-BXD_GROUP_EXCEPTIONS = ['BXD-Longevity', 'BXD-AE', 'BXD-Heart-Metals', 'BXD-NIA-AD', 'BXD-JAX-OFS', 'BXD-MBD-UTHSC']
+BXD_GROUP_EXCEPTIONS = ['BXD-Longevity', 'BXD-AE', 'BXD-Heart-Metals', 'BXD-NIA-AD', 'BXD-JAX-OFS', 'BXD-MBD-UTHSC', 'BXD_UT-SJ']
 
 # This is used to avoid running the code that manually checks which samples aren't part of the "main" sample list (derived from the .geno file)
 # The code in question is very time-consuming, and unnecessary for the vast majority of groups
